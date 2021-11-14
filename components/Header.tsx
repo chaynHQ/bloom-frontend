333e79537ca78661957e03c0a7e5f88ecd0c4ce1
annarhughes <annaraehughes@live.co.uk>
2021-11-14 23:23:14 +0000

    header component

---
+++ components/Header.tsx
@@ -0,0 +1,55 @@
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Image from 'next/image';
+import * as React from 'react';
+
+interface HeaderProps {
+  title:
+    | string
+    | React.ReactNodeArray
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+  introduction:
+    | string
+    | React.ReactNodeArray
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>;
+  imageSrc: StaticImageData;
+  imageAlt: string;
+}
+
+const headerContainerStyles = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+} as const;
+
+const imageContainerStyle = {
+  position: 'relative',
+  width: { xs: 150, md: 250 },
+  height: { xs: 150, md: 250 },
+} as const;
+const textContainerStyle = {
+  maxWidth: 600,
+} as const;
+
+const Header = (props: HeaderProps) => {
+  const { title, introduction, imageAlt, imageSrc } = props;
+
+  return (
+    <Container sx={headerContainerStyles}>
+      <Box sx={textContainerStyle}>
+        <Typography variant="h1" component="h1" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body1" component="p" gutterBottom>
+          {introduction}
+        </Typography>
+      </Box>
+      <Box sx={imageContainerStyle}>
+        <Image alt={imageAlt} src={imageSrc} layout="fill" objectFit="contain" />
+      </Box>
+    </Container>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,52 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import type { NextPage } from 'next';
 import { GetStaticPropsContext } from 'next';
 import { useTranslations } from 'next-intl';
+import Header from '../components/Header';
 import Link from '../components/Link';
+import bloomLogo from '../public/bloom_logo.png';
 
 const Home: NextPage = () => {
   const t = useTranslations('Index');
 
-  return (
-    <Container>
-      <Typography variant="h1" component="h1" gutterBottom>
-        H1 -{' '}
-        {t.rich('introduction', {
-          nextLink: (children) => <Link href="https://redux.js.org/">{children}</Link>,
-          reduxLink: (children) => <Link href="https://redux.js.org/">{children}</Link>,
-          muiLink: (children) => <Link href="https://mui.com/">{children}</Link>,
-        })}
-      </Typography>
+  const headerProps = {
+    title: t.rich('title'),
+    introduction: t.rich('introduction', {
+      nextLink: (children) => <Link href="https://redux.js.org/">{children}</Link>,
+      reduxLink: (children) => <Link href="https://redux.js.org/">{children}</Link>,
+      muiLink: (children) => <Link href="https://mui.com/">{children}</Link>,
+    }),
+    imageSrc: bloomLogo,
+    imageAlt: 'Bloom logo',
+  };
 
-      <Typography variant="body1" component="p" gutterBottom>
-        {t.rich('getStarted', {
-          code: (children) => <code>{children}</code>,
-        })}
-      </Typography>
-      <Button
-        sx={{ mt: 2, mr: 1.5 }}
-        variant="contained"
-        component={Link}
-        href="https://nextjs.org/docs"
-      >
-        {t.rich('nextDocs')}
-      </Button>
-      <Box sx={{ bgcolor: 'primary.main', height: '2.5rem', mt: 2.5 }}></Box>
-      <Box sx={{ bgcolor: 'primary.light', height: '2.5rem' }}></Box>
-      <Box sx={{ bgcolor: 'primary.dark', height: '2.5rem' }}></Box>
-      <Box sx={{ bgcolor: 'secondary.main', height: '2.5rem' }}></Box>
-      <Box sx={{ bgcolor: 'secondary.light', height: '2.5rem' }}></Box>
-      <Box sx={{ bgcolor: 'secondary.dark', height: '2.5rem' }}></Box>
-    </Container>
+  return (
+    <Box>
+      <Header
+        title={headerProps.title}
+        introduction={headerProps.introduction}
+        imageSrc={headerProps.imageSrc}
+        imageAlt={headerProps.imageAlt}
+      />
+      <Container>
+        <Button
+          sx={{ mt: 2, mr: 1.5 }}
+          variant="contained"
+          component={Link}
+          href="https://nextjs.org/docs"
+        >
+          {t.rich('nextDocs')}
+        </Button>
+        <Box sx={{ bgcolor: 'primary.main', height: '2.5rem', mt: 2.5 }}></Box>
+        <Box sx={{ bgcolor: 'primary.light', height: '2.5rem' }}></Box>
+        <Box sx={{ bgcolor: 'primary.dark', height: '2.5rem' }}></Box>
+        <Box sx={{ bgcolor: 'secondary.main', height: '2.5rem' }}></Box>
+        <Box sx={{ bgcolor: 'secondary.light', height: '2.5rem' }}></Box>
+        <Box sx={{ bgcolor: 'secondary.dark', height: '2.5rem' }}></Box>
+      </Container>
+    </Box>
   );
 };
 
