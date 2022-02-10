553317757078887d202b420b1236cdc6b3c15901
annarhughes <annaraehughes@live.co.uk>
2022-02-10 17:18:46 +0000

    fix build + tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { GetStaticPropsContext } from 'next';
 import { useTranslations } from 'next-intl';
 import Header from '../components/Header';
 import Link from '../components/Link';
-import illustrationPerson3Pink from '../public/illustration_person3_pink.png';
+import illustrationPerson3Pink from '../public/illustration_person3_pink.svg';
 
 const Home: NextPage = () => {
   const t = useTranslations('Index');
