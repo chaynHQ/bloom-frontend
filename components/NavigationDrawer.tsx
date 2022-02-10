bd994ada89beff872b766be771973d91fe7ae582
Anna Hughes <annaraehughes@live.co.uk>
2022-02-10 17:21:06 +0000

    fix build + tweaks (#93)

--- components/NavigationDrawer.tsx
+++ components/NavigationDrawer.tsx
@@ -7,7 +7,7 @@ import Drawer from '@mui/material/Drawer';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import * as React from 'react';
-import bloomLogo from '../public/bloom_logo.png';
+import bloomLogo from '../public/bloom_logo.svg';
 import Link from './Link';
 import NavigationMenu from './NavigationMenu';
 
