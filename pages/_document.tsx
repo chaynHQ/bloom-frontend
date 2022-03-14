9418326773ad1bd791b2f600266efa4e931c64c5
swetha-chayn <swetha@chayn.co>
2022-03-14 20:44:37 +0000

    Update _document.tsx with PWA tags
    Added link to manifest document as well as the apple touch icon. iOS devices are expected to scale the given image as necessary^.
    
    ^ https://stackoverflow.com/questions/16423152/what-size-is-apple-touch-icon-when-no-size-specified
    
    9322 / Add PWA support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,9 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* PWA primary color */}
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/apple/icon-120x120.png"></link>
+          {/* PWA primary color TODO move within tag*/}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap"
