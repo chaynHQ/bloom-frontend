97bc93d3c873836c54b4295c1e5d89b18128c5e8
annarhughes <annaraehughes@live.co.uk>
2022-03-15 01:18:01 +0000

    public welcome page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,131 @@
+import { Button } from '@mui/material';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import type { NextPage } from 'next';
 import { GetStaticPropsContext } from 'next';
 import { useTranslations } from 'next-intl';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { StoriesParams, StoryData } from 'storyblok-js-client';
 import Link from '../components/common/Link';
-import Header from '../components/layout/Header';
-import illustrationPerson3Pink from '../public/illustration_person3_pink.svg';
+import PartnerHeader from '../components/layout/PartnerHeader';
+import StoryblokPageSection from '../components/storyblok/StoryblokPageSection';
+import Storyblok, { useStoryblok } from '../config/storyblok';
+import { LANGUAGES } from '../constants/enums';
+import illustrationBloomHeadYellow from '../public/illustration_bloom_head_yellow.svg';
+import welcomeToBloom from '../public/welcome_to_bloom.svg';
+import { rowStyle } from '../styles/common';
 
-const Home: NextPage = () => {
-  const t = useTranslations('Index');
+const textContainerStyle = {
+  maxWidth: 600,
+  width: { xs: '100%', md: '45%' },
+} as const;
+
+const rowItem = {
+  width: { xs: '100%', sm: '60%', md: '45%' },
+} as const;
+
+interface Props {
+  story: StoryData;
+  preview: boolean;
+  sbParams: StoriesParams;
+  messages: any;
+  locale: LANGUAGES;
+}
+
+const Welcome: NextPage<Props> = ({ story, preview, sbParams, messages, locale }) => {
+  const t = useTranslations('Welcome');
+  const router = useRouter();
+  const [codeParam, setCodeParam] = useState<string>('');
+
+  story = useStoryblok(story, preview, sbParams, locale);
 
   const headerProps = {
-    title: t('title'),
-    introduction: t('introduction', {
-      nextLink: (children) => <Link href="https://redux.js.org/">{children}</Link>,
-      reduxLink: (children) => <Link href="https://redux.js.org/">{children}</Link>,
-      muiLink: (children) => <Link href="https://mui.com/">{children}</Link>,
-    }),
-    imageSrc: illustrationPerson3Pink,
-    imageAlt: 'alt.personSitting',
+    partnerLogoSrc: welcomeToBloom,
+    partnerLogoAlt: 'alt.welcomeToBloom',
+    imageSrc: illustrationBloomHeadYellow,
+    imageAlt: 'alt.bloomHead',
   };
+
+  useEffect(() => {
+    const { code } = router.query;
+    if (code) setCodeParam(code + '');
+  }, [setCodeParam, router.query]);
 
   return (
     <Box>
-      <Header
-        title={headerProps.title}
-        introduction={headerProps.introduction}
+      <Head>
+        <title>{story.content.title}</title>
+      </Head>
+      <PartnerHeader
+        partnerLogoSrc={headerProps.partnerLogoSrc}
+        partnerLogoAlt={headerProps.partnerLogoAlt}
         imageSrc={headerProps.imageSrc}
         imageAlt={headerProps.imageAlt}
       />
-      <Container>
-        <Button
-          sx={{ mt: 2, mr: 1.5 }}
-          variant="contained"
-          component={Link}
-          href="https://nextjs.org/docs"
-        >
-          {t('nextDocs')}
-        </Button>
-        <Box sx={{ bgcolor: 'primary.main', height: '2.5rem', mt: 2.5 }}></Box>
-        <Box sx={{ bgcolor: 'primary.light', height: '2.5rem' }}></Box>
-        <Box sx={{ bgcolor: 'primary.dark', height: '2.5rem' }}></Box>
-        <Box sx={{ bgcolor: 'secondary.main', height: '2.5rem' }}></Box>
-        <Box sx={{ bgcolor: 'secondary.light', height: '2.5rem' }}></Box>
-        <Box sx={{ bgcolor: 'secondary.dark', height: '2.5rem' }}></Box>
+      <Container sx={{ ...rowStyle, backgroundColor: 'primary.light' }}>
+        <Box sx={textContainerStyle}>
+          <Typography pb={2} variant="subtitle1" component="p">
+            {story.content.introduction}
+          </Typography>
+        </Box>
+        <Card sx={rowItem}>
+          <CardContent>
+            <Typography variant="h2" component="h2">
+              {t('getStarted')}
+            </Typography>
+            <Typography>{t('publicIntroduction')}</Typography>
+            <Button
+              sx={{ mt: 3 }}
+              variant="contained"
+              fullWidth
+              component={Link}
+              color="secondary"
+              href="/auth/register"
+            >
+              {t('getStarted')}
+            </Button>
+          </CardContent>
+        </Card>
       </Container>
+      {story.content.page_sections?.length > 0 &&
+        story.content.page_sections.map((section: any, index: number) => (
+          <StoryblokPageSection
+            key={`page_section_${index}`}
+            content={section.content}
+            alignment={section.alignment}
+            color={section.color}
+          />
+        ))}
     </Box>
   );
 };
 
-export function getStaticProps({ locale }: GetStaticPropsContext) {
+export async function getStaticProps({ locale, preview = false, params }: GetStaticPropsContext) {
+  const sbParams = {
+    version: preview ? 'draft' : 'published',
+    cv: preview ? Date.now() : 0,
+  };
+
+  let { data } = await Storyblok.get(`cdn/stories/welcome`, sbParams);
+
   return {
     props: {
+      story: data ? data.story : null,
+      preview,
       messages: {
         ...require(`../messages/shared/${locale}.json`),
         ...require(`../messages/navigation/${locale}.json`),
-        ...require(`../messages/index/${locale}.json`),
+        ...require(`../messages/welcome/${locale}.json`),
       },
+      locale,
     },
+    revalidate: 3600, // revalidate every hour
   };
 }
 
-export default Home;
+export default Welcome;
