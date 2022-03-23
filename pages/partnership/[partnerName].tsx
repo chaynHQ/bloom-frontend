d586f13889d3ffe06df9bc23faa3ba92705c4f9a
swetha-chayn <swetha@chayn.co>
2022-03-23 19:31:58 +0000

    Allow partnership page access to Storyblok data
    Method getStaticProps brings in data from Storyblok.
    This is then available to the Partnership component via props.
    
    The method getStaticPaths provides paths to be prerendered by Next.
    Storyblok will have the expected paths (i.e. partners) and the content that
    goes with each path.

--- pages/partnership/[partnerName].tsx
+++ pages/partnership/[partnerName].tsx
@@ -1,5 +1,8 @@
-import { NextPage } from 'next';
+import Box from '@mui/material/Box';
+import { GetStaticPathsContext, GetStaticPropsContext, NextPage } from 'next';
+import Head from 'next/head';
 import { StoriesParams, StoryData } from 'storyblok-js-client';
+import Storyblok, { useStoryblok } from '../../config/storyblok';
 import { LANGUAGES } from '../../constants/enums';
 
 interface Props {
@@ -9,14 +12,68 @@ interface Props {
   locale: LANGUAGES;
 }
 
-const Partnership: NextPage<Props> = () => {
+const Partnership: NextPage<Props> = ({ story, preview, sbParams, locale }) => {
   // TODO translations
 
-  return <></>;
+  story = useStoryblok(story, preview, sbParams, locale);
+
+  // TODO add in authenticated & partner affiliated access
+  // TODO add in actual story content
+  return (
+    <Box>
+      <Head>
+        <title>{story.content.title}</title>
+      </Head>
+    </Box>
+  );
 };
 
-// TODO getStaticProps: retrieve storyblok data
+export async function getStaticProps({ locale, preview = false, params }: GetStaticPropsContext) {
+  const partnerName = params?.partnerName;
 
-// TODO getStaticPaths: get dynamic paths
+  const sbParams = {
+    version: preview ? 'draft' : 'published',
+    cv: preview ? Date.now() : 0,
+    language: locale,
+  };
+
+  let { data } = await Storyblok.get(`cdn/stories/partnership/${partnerName}`, sbParams);
+
+  return {
+    props: {
+      story: data ? data.story : null,
+      preview,
+      messages: {
+        ...require(`../../messages/shared/${locale}.json`),
+        ...require(`../../messages/navigation/${locale}.json`),
+      },
+      locale,
+    },
+    revalidate: 3600, // revalidate every hour
+  };
+}
+
+export async function getStaticPaths({ locales }: GetStaticPathsContext) {
+  let { data } = await Storyblok.get('cdn/links/?starts_with=partnership/');
+
+  let paths: any = [];
+  Object.keys(data.links).forEach((linkKey) => {
+    // get array for slug because of catch all
+    const slug = data.links[linkKey].slug;
+    let splittedSlug = slug.split('/');
+
+    if (locales) {
+      // create additional languages
+      for (const locale of locales) {
+        paths.push({ params: { partnerName: splittedSlug[1] }, locale });
+      }
+    }
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export default Partnership;
