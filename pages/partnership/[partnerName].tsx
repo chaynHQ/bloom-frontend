a99b95e8a05c604e37331d40e14d3a1cbe7e6c5d
swetha-chayn <swetha@chayn.co>
2022-03-23 22:35:23 +0000

    Restrict partnership content to affiliated users

--- pages/partnership/[partnerName].tsx
+++ pages/partnership/[partnerName].tsx
@@ -1,14 +1,20 @@
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import { GetStaticPathsContext, GetStaticPropsContext, NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 import { StoriesParams, StoryData } from 'storyblok-js-client';
+import { RootState } from '../../app/store';
 import PartnerHeader from '../../components/layout/PartnerHeader';
 import StoryblokPageSection from '../../components/storyblok/StoryblokPageSection';
 import Storyblok, { useStoryblok } from '../../config/storyblok';
 import { LANGUAGES } from '../../constants/enums';
 import { getPartnerContent } from '../../constants/partners';
+import { useTypedSelector } from '../../hooks/store';
 import illustrationBloomHeadYellow from '../../public/illustration_bloom_head_yellow.svg';
+import illustrationPerson4Peach from '../../public/illustration_person4_peach.svg';
 import welcomeToBloom from '../../public/welcome_to_bloom.svg';
+import { columnStyle } from '../../styles/common';
 
 interface Props {
   story: StoryData;
@@ -17,12 +23,28 @@ interface Props {
   locale: LANGUAGES;
 }
 
+// TODO pull out shared stylings
+const accessContainerStyle = {
+  ...columnStyle,
+  height: '100vh',
+} as const;
+
+const imageContainerStyle = {
+  position: 'relative',
+  width: { xs: 150, md: 210 },
+  height: { xs: 150, md: 210 },
+  marginBottom: 4,
+} as const;
+
 const Partnership: NextPage<Props> = ({ story, preview, sbParams, locale }) => {
   // TODO translations
 
   story = useStoryblok(story, preview, sbParams, locale);
-  const partnerContent = getPartnerContent(story.slug);
 
+  const partnerName = story.slug;
+  const partnerContent = getPartnerContent(partnerName);
+
+  // TODO reduce duplication: pull out content
   const headerProps = {
     partnerLogoSrc: partnerContent.partnershipLogo || welcomeToBloom,
     partnerLogoAlt: partnerContent.partnershipLogoAlt || 'alt.welcomeToBloom',
@@ -30,28 +52,54 @@ const Partnership: NextPage<Props> = ({ story, preview, sbParams, locale }) => {
     imageAlt: 'alt.bloomHead',
   };
 
-  // TODO add in authenticated & partner affiliated access
+  // TODO useEffect for partnerAccess?
+
+  const { partnerAccesses } = useTypedSelector((state: RootState) => state);
+
+  const hasPartnerAccess = partnerAccesses.find(
+    (partnerAccess) => partnerAccess.partner.name === partnerName,
+  );
+
+  if (hasPartnerAccess) {
+    // can view partnership content
+    return (
+      <Box>
+        <Head>
+          <title>{story.content.title}</title>
+        </Head>
+        <PartnerHeader
+          partnerLogoSrc={headerProps.partnerLogoSrc}
+          partnerLogoAlt={headerProps.partnerLogoAlt}
+          imageSrc={headerProps.imageSrc}
+          imageAlt={headerProps.imageAlt}
+        />
+        {story.content.page_sections?.length > 0 &&
+          story.content.page_sections.map((section: any, index: number) => (
+            <StoryblokPageSection
+              key={`page_section_${index}`}
+              content={section.content}
+              alignment={section.alignment}
+              color={section.color}
+            />
+          ))}
+      </Box>
+    );
+  }
+
+  // if user doesn't have partner access, display alternate message
   return (
-    <Box>
-      <Head>
-        <title>{story.content.title}</title>
-      </Head>
-      <PartnerHeader
-        partnerLogoSrc={headerProps.partnerLogoSrc}
-        partnerLogoAlt={headerProps.partnerLogoAlt}
-        imageSrc={headerProps.imageSrc}
-        imageAlt={headerProps.imageAlt}
-      />
-      {story.content.page_sections?.length > 0 &&
-        story.content.page_sections.map((section: any, index: number) => (
-          <StoryblokPageSection
-            key={`page_section_${index}`}
-            content={section.content}
-            alignment={section.alignment}
-            color={section.color}
-          />
-        ))}
-    </Box>
+    // TODO pull out access guard message component
+    <Container sx={accessContainerStyle}>
+      <Box sx={imageContainerStyle}>
+        <Image
+          alt={'alt.personTea'}
+          src={illustrationPerson4Peach}
+          layout="fill"
+          objectFit="contain"
+        />
+      </Box>
+      {/* TODO add message  */}
+    </Container>
   );
 };
 
