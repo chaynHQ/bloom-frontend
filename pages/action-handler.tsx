cebc83f04ec2a16ca984344df5a5b42c73efe149
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-05-05 15:34:45 +0100

    Pass language param into action handler

--- pages/action-handler.tsx
+++ pages/action-handler.tsx
@@ -1,6 +1,7 @@
 import { GetStaticPropsContext, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import LoadingContainer from '../components/common/LoadingContainer';
+import { LANGUAGES } from '../constants/enums';
 
 // Page to handle redirects from external tools. E.g. firebase auth emails redirect to /action-handler?mode=resetPassword&oobCode....
 const ActionHandler: NextPage = () => {
@@ -8,10 +9,13 @@ const ActionHandler: NextPage = () => {
 
   if (router.isReady) {
     let modeParam = router.query.mode;
+    let langParam = router.query.lang;
+    const options =
+      typeof langParam === 'string' && langParam in LANGUAGES ? { locale: langParam } : undefined;
     if (modeParam && modeParam === 'resetPassword') {
-      router.replace(`/auth/reset-password?oobCode=${router.query.oobCode}`);
+      router.replace(`/auth/reset-password?oobCode=${router.query.oobCode}`, undefined, options);
     } else {
-      router.replace('/404');
+      router.replace('/404', undefined, options);
     }
   }
   return <LoadingContainer />;
