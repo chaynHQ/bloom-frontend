04fb954fb43270d992e24894c300142651820768
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-06-05 12:35:58 +0100

    Add account type to analytics meta data

---
+++ utils/getAccountType.ts
@@ -0,0 +1,23 @@
+import { PartnerAccesses } from '../app/partnerAccessSlice';
+import { PartnerAdmin } from '../app/partnerAdminSlice';
+
+export enum AccountType {
+  partnerAdmin = 'PARTNER_ADMIN',
+  superAdmin = 'SUPER_ADMIN',
+  publicUser = 'PUBLIC_USER',
+  partnerUser = 'PARTNER_USER',
+}
+export const getAccountType = (
+  partnerAdmin: PartnerAdmin | undefined,
+  partnerAccesses: PartnerAccesses | undefined,
+): AccountType => {
+  // TODO We do not have the data for whether someone is a super admin yet
+  // this would be great to implement
+  if (partnerAdmin) {
+    return AccountType.partnerAdmin;
+  }
+  if (partnerAccesses && partnerAccesses.length > 0) {
+    return AccountType.partnerUser;
+  }
+  return AccountType.publicUser;
+};
