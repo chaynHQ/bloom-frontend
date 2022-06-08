b7984215efb3f7bdb298f9fa0191c62ba120183a
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-06-08 08:30:55 +0100

    Remove account type from test name

--- utils/getAccountType.test.ts
+++ utils/getAccountType.test.ts
@@ -5,16 +5,16 @@ const partnerAdmin = {} as PartnerAdmin;
 const partnerAccesses = [{} as PartnerAccess] as PartnerAccesses;
 
 describe('getAccountType', () => {
-  it('getAccountType should return publicUser if neither is partnerAdmin nor partner user', () => {
+  it('should return publicUser if neither is partnerAdmin nor partner user', () => {
     expect(getAccountType(undefined, undefined)).toBe(AccountType.publicUser);
   });
-  it('getAccountType should return partnerAdmin if is partnerAdmin', () => {
+  it('should return partnerAdmin if is partnerAdmin', () => {
     expect(getAccountType(partnerAdmin, undefined)).toBe(AccountType.partnerAdmin);
   });
-  it('getAccountType should return partnerUser if not partnerAdmin and connected to partner', () => {
+  it('should return partnerUser if not partnerAdmin and connected to partner', () => {
     expect(getAccountType(undefined, partnerAccesses)).toBe(AccountType.partnerUser);
   });
-  it('getAccountType should return partnerAdmin  if both partnerAdmin and connected to partner', () => {
+  it('should return partnerAdmin  if both partnerAdmin and connected to partner', () => {
     expect(getAccountType(partnerAdmin, partnerAccesses)).toBe(AccountType.partnerAdmin);
   });
 });
