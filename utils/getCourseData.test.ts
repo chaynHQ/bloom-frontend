21d43e19b7d02fca4c9ab5475d5f6e039434e35e
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-06-28 17:12:54 +0100

    Add course data to survey forms

---
+++ utils/getCourseData.test.ts
@@ -0,0 +1,25 @@
+import { Course, Courses, Session } from '../app/coursesSlice';
+import { getCourseData } from './getCourseData';
+
+const coursesBase: Courses = [
+  {
+    storyblokId: 98127815,
+    sessions: [{} as Session],
+  } as Course,
+];
+describe('getCourseData', () => {
+  it('if empty course is supplied, it returns empty record ', () => {
+    expect(getCourseData([])).toEqual({});
+  });
+  it('if course is supplied, it returns record ', () => {
+    expect(getCourseData(coursesBase)).toEqual({ number_dbr_sessions: 1 });
+  });
+  it('if two courses are supplied, it returns record with two entries', () => {
+    expect(
+      getCourseData(coursesBase.concat({ storyblokId: 100421614, sessions: [{}, {}] } as Course)),
+    ).toEqual({
+      number_dbr_sessions: 1,
+      number_spst_sessions: 2,
+    });
+  });
+});
