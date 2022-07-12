f95b8ac7d7581e73ece705984e87d5ba4d8c65b7
swetha-chayn <swetha@chayn.co>
2022-07-12 19:01:13 +0100

    Fix path of image url to remove excess forward slash
    120722-1323 / Add meta-data for socials image preview

--- components/head/OpenGraphMetadata.tsx
+++ components/head/OpenGraphMetadata.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 const descriptionContent =
   "Learn and heal from trauma in a private, supportive space. Our courses are written and checked by survivors, allies, mental health support workers and therapists from around the world. Healing from trauma can be isolating. We're here for you.";
-const imageContent = `${process.env.NEXT_PUBLIC_BASE_URL}/bloom_socials_preview.png`;
+const imageContent = `${process.env.NEXT_PUBLIC_BASE_URL}bloom_socials_preview.png`;
 const imageAltContent =
   "An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word 'Bloom' hovers above the person.";
 
