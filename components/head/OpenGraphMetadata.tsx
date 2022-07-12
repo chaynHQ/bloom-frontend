07fae4c2171cc15d2da6d03abe2347f0a405140c
swetha-chayn <swetha@chayn.co>
2022-07-12 18:52:31 +0100

    Add an absolute URL for og:image metadata
    120722-1323 / Add meta-data for socials image preview

--- components/head/OpenGraphMetadata.tsx
+++ components/head/OpenGraphMetadata.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 const descriptionContent =
   "Learn and heal from trauma in a private, supportive space. Our courses are written and checked by survivors, allies, mental health support workers and therapists from around the world. Healing from trauma can be isolating. We're here for you.";
+const imageContent = `${process.env.NEXT_PUBLIC_BASE_URL}/bloom_socials_preview.png`;
 const imageAltContent =
   "An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word 'Bloom' hovers above the person.";
 
@@ -10,7 +11,7 @@ const OpenGraphMetadata = () => {
     <Head>
       <meta property="og:title" content="Bloom" key="og-title" />
       <meta property="og:description" content={descriptionContent} key="og-description" />
-      <meta property="og:image" content="/bloom_socials_preview.png" key="og-image"></meta>
+      <meta property="og:image" content={imageContent} key="og-image"></meta>
       <meta property="og:image:alt" content={imageAltContent} key="og-image-alt"></meta>
     </Head>
   );
