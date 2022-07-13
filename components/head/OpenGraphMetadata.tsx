c4ae251778b798163d04a88e9893804df73ccc50
swetha-chayn <swetha@chayn.co>
2022-07-13 19:14:21 +0100

    Update twitter descriptiojn to be short
    120722-1323 / Add meta-data for socials image preview

--- components/head/OpenGraphMetadata.tsx
+++ components/head/OpenGraphMetadata.tsx
@@ -3,10 +3,10 @@ import Head from 'next/head';
 const descriptionContent =
   "Learn and heal from trauma in a private, supportive space. Our courses are written and checked by survivors, allies, mental health support workers and therapists from around the world. Healing from trauma can be isolating. We're here for you.";
 const twitterDescriptionContent =
-  "Learn & heal from trauma in a private, supportive space. Our courses are written & checked by survivors, allies, mental health support workers and therapists. We're here for you.";
+  'A global tech nonprofit empowering women & marginalised genders facing abuse. Feminist, Survivor-led, Intersectional.';
 const imageContent = `${process.env.NEXT_PUBLIC_BASE_URL}bloom_socials_preview.png`;
 const imageAltContent =
-  "An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word 'Bloom' hovers above the person.";
+  'An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word "Bloom" hovers above the person.';
 
 const OpenGraphMetadata = () => {
   return (
