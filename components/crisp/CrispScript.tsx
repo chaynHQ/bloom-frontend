4256f83f97f05fafded5d2d9b963376a2066ac14
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-07-18 14:17:41 +0100

    Fix crisp issue where it doesn't clear chat on log out

--- components/crisp/CrispScript.tsx
+++ components/crisp/CrispScript.tsx
@@ -1,5 +1,5 @@
 import Script from 'next/script';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { RootState } from '../../app/store';
 import { CHAT_MESSAGE_SENT, CHAT_STARTED, FIRST_CHAT_STARTED } from '../../constants/events';
 import { useTypedSelector } from '../../hooks/store';
@@ -9,60 +9,46 @@ const CrispScript = () => {
   const { user, partnerAccesses, partnerAdmin } = useTypedSelector((state: RootState) => state);
 
   const eventData = getEventUserData({ user, partnerAccesses, partnerAdmin });
-  const [showLiveChat, setShowLiveChat] = useState<boolean>(false);
 
   useEffect(() => {
-    const liveAccess = partnerAccesses.find(
-      (partnerAccess) => partnerAccess.featureLiveChat === true,
-    );
-    if (user.email && liveAccess) {
-      setShowLiveChat(true);
-    } else {
-      setShowLiveChat(false);
+    if (process.env.NEXT_PUBLIC_ENV === 'production') {
+      (window as any).$crisp.push(['safe', true]);
     }
-  }, [user, partnerAccesses]);
-
-  useEffect(() => {
-    if ((window as any).$crisp && (window as any).$crisp.push) {
-      if (process.env.NEXT_PUBLIC_ENV === 'production') {
-        (window as any).$crisp.push(['safe', true]);
-      }
-      (window as any).$crisp.push(['do', 'chat:hide']);
-      (window as any).$crisp.push([
-        'on',
-        'chat:initiated',
-        () => {
-          logEvent(FIRST_CHAT_STARTED, eventData);
-        },
-      ]);
-      (window as any).$crisp.push([
-        'on',
-        'message:sent',
-        () => {
-          logEvent(CHAT_MESSAGE_SENT, eventData);
-        },
-      ]);
-      (window as any).$crisp.push([
-        'on',
-        'chat:opened',
-        () => {
-          logEvent(CHAT_STARTED, eventData);
-        },
-      ]);
-    }
+    (window as any).$crisp.push([
+      'on',
+      'chat:initiated',
+      () => {
+        logEvent(FIRST_CHAT_STARTED, eventData);
+      },
+    ]);
+    (window as any).$crisp.push([
+      'on',
+      'message:sent',
+      () => {
+        logEvent(CHAT_MESSAGE_SENT, eventData);
+      },
+    ]);
+    (window as any).$crisp.push([
+      'on',
+      'chat:opened',
+      () => {
+        logEvent(CHAT_STARTED, eventData);
+      },
+    ]);
   }, []);
 
   useEffect(() => {
-    if ((window as any).$crisp && (window as any).$crisp.is) {
-      if (showLiveChat) {
-        (window as any).$crisp.push(['set', 'user:email', [user.email]]);
-        (window as any).$crisp.push(['do', 'chat:show']);
-      } else {
-        (window as any).$crisp.push(['do', 'chat:hide']);
-        (window as any).$crisp.push(['do', 'session:reset']);
-      }
+    const hasLiveAccess =
+      partnerAccesses.length === 0
+        ? true
+        : partnerAccesses.find((partnerAccess) => partnerAccess.featureLiveChat === true);
+    if (user.email && hasLiveAccess) {
+      (window as any).$crisp.push(['set', 'user:email', [user.email]]);
+      (window as any).$crisp.push(['do', 'chat:show']);
+    } else {
+      (window as any).$crisp.push(['do', 'chat:hide']);
     }
-  }, [showLiveChat, user]);
+  }, [user, partnerAccesses]);
 
   const crispScriptUrl = 'https://client.crisp.chat/l.js';
 
