79e7fccf15c31f72a5e1f510a9abc6c04821a1f2
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-08-14 12:01:00 +0100

    Send events via gtag

--- components/head/GoogleTagManagerScript.tsx
+++ components/head/GoogleTagManagerScript.tsx
@@ -20,7 +20,12 @@ const GoogleTagManagerScript = () => {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');`,
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
+            gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}');
+
+
+            
+            `,
       }}
     />
   );
