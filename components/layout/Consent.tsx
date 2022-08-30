38ccdb1ec874ee3705f8c3072527b2752973a637
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-08-30 09:03:37 +0100

    Remove styling from container of consent

--- components/layout/Consent.tsx
+++ components/layout/Consent.tsx
@@ -1,4 +1,5 @@
 import { alpha, Box, Button, useTheme } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 import React from 'react';
@@ -15,30 +16,25 @@ const Consent = (props: {}) => {
   const { user, partnerAccesses, partnerAdmin } = useTypedSelector((state: RootState) => state);
   const eventUserData = getEventUserData({ user, partnerAccesses, partnerAdmin });
   const tS = useTranslations('Shared');
+  const isMobileScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const consentBoxStyle: React.CSSProperties = {
-    backgroundColor: 'secondary.light',
+    backgroundColor: theme.palette.secondary.light,
     maxWidth: theme.spacing(50),
     maxHeight: theme.spacing(35),
     position: 'fixed',
-    left: theme.spacing(1),
-    bottom: theme.spacing(1),
+    left: isMobileScreen ? theme.spacing(1) : theme.spacing(2),
+    bottom: isMobileScreen ? theme.spacing(1) : theme.spacing(2),
     borderRadius: theme.spacing(2),
     boxShadow: `${alpha(theme.palette.common.black, 0.2)} 0px ${theme.spacing(1)} ${theme.spacing(
       4,
     )} 0px`,
     textAlign: 'center',
     lineHeight: 1.5,
-    marginRight: theme.spacing(1),
-    padding: `${theme.spacing(1)} ${theme.spacing(3)} ${theme.spacing(1.5)} ${theme.spacing(3)}`,
-    fontSize: theme.typography.body2.fontSize,
-    [theme.breakpoints.up('sm')]: {
-      fontSize: theme.typography.body1.fontSize,
-      padding: `${theme.spacing(2)} ${theme.spacing(4)}`,
-      left: theme.spacing(2),
-      bottom: theme.spacing(2),
-      marginRight: theme.spacing(2),
-    },
+    marginRight: isMobileScreen ? theme.spacing(1) : theme.spacing(2),
+    padding: isMobileScreen
+      ? `${theme.spacing(1)} ${theme.spacing(3)}`
+      : `${theme.spacing(2)} ${theme.spacing(4)}`,
   };
   const acceptButtonStyle = {
     backgroundColor: 'secondary.main',
@@ -68,46 +64,53 @@ const Consent = (props: {}) => {
   };
 
   return (
-    <Box sx={consentBoxStyle}>
-      <CookieConsent
-        disableStyles={true}
-        buttonText={tS('cookieConsent.acceptLabel')}
-        declineButtonText={tS('cookieConsent.declineLabel')}
-        cookieName="analyticsConsent"
-        location="none"
-        expires={365}
-        onAccept={handleAccept}
-        onDecline={handleDecline}
-        enableDeclineButton
-        ButtonComponent={Button}
-        customButtonProps={{
-          size: 'medium',
-          variant: 'contained',
-          display: 'block',
-          qaId: 'cookieConsentAcceptButton',
-          sx: acceptButtonStyle,
+    <CookieConsent
+      style={consentBoxStyle}
+      disableStyles={true}
+      buttonText={tS('cookieConsent.acceptLabel')}
+      declineButtonText={tS('cookieConsent.declineLabel')}
+      cookieName="analyticsConsent"
+      location="none"
+      expires={365}
+      onAccept={handleAccept}
+      onDecline={handleDecline}
+      enableDeclineButton
+      ButtonComponent={Button}
+      customButtonProps={{
+        size: 'medium',
+        variant: 'contained',
+        display: 'block',
+        qaId: 'cookieConsentAcceptButton',
+        sx: acceptButtonStyle,
+      }}
+      customDeclineButtonProps={{
+        size: 'small',
+        variant: 'text',
+        qaId: 'cookieConsentDeclineButton',
+        sx: declineButtonStyle,
+      }}
+      ariaAcceptLabel={tS('cookieConsent.acceptLabel')}
+      ariaDeclineLabel={tS('cookieConsent.declineLabel')}
+      flipButtons={true}
+    >
+      <Box width={[60, 70]} margin="auto" marginBottom={2}>
+        <Image alt={tS('alt.cookieCat')} src={IllustrationCookieCat} layout="responsive" />
+      </Box>
+      <Box
+        marginBottom={2}
+        sx={{
+          fontSize: theme.typography.body2.fontSize,
+          [theme.breakpoints.up('sm')]: {
+            fontSize: theme.typography.body1.fontSize,
+          },
         }}
-        customDeclineButtonProps={{
-          size: 'small',
-          variant: 'text',
-          qaId: 'cookieConsentDeclineButton',
-          sx: declineButtonStyle,
-        }}
-        ariaAcceptLabel={tS('cookieConsent.acceptLabel')}
-        ariaDeclineLabel={tS('cookieConsent.declineLabel')}
-        flipButtons={true}
       >
-        <Box width={[60, 70]} margin="auto" marginBottom={2}>
-          <Image alt={tS('alt.cookieCat')} src={IllustrationCookieCat} layout="responsive" />
-        </Box>
-        <Box marginBottom={2}>
-          {tS('cookieConsent.cookieConsentExplainer')}
-          <Link href="https://chayn.notion.site/Cookie-Policy-e478b184ea6a4002ba660d052f332c5a">
-            {tS('cookieConsent.cookieConsentPolicy')}
-          </Link>
-        </Box>
-      </CookieConsent>
-    </Box>
+        {tS('cookieConsent.cookieConsentExplainer')}
+        <Link href="https://chayn.notion.site/Cookie-Policy-e478b184ea6a4002ba660d052f332c5a">
+          {tS('cookieConsent.cookieConsentPolicy')}
+        </Link>
+      </Box>
+    </CookieConsent>
   );
 };
 
