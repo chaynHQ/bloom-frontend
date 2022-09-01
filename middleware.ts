29fac99d713b8f47e9b4042a64ec48ce39a1ffa4
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-09-01 15:09:02 +0100

    Rename ENVIRONMENT enum and move file

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,9 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-
-enum Environment {
-  PRODUCTION = 'production',
-  LOCAL = 'local',
-  STAGING = 'staging',
-}
+import { ENVIRONMENT } from './constants/enums';
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const environments = [Environment.STAGING, Environment.PRODUCTION];
-  const currentEnv = process.env.NEXT_PUBLIC_ENV as Environment;
+  const environments = [ENVIRONMENT.STAGING, ENVIRONMENT.PRODUCTION];
+  const currentEnv = process.env.NEXT_PUBLIC_ENV as ENVIRONMENT;
 
   if (environments.includes(currentEnv) && req.headers.get('x-forwarded-proto') !== 'https') {
     const hostname = req.headers.get('host') || req.nextUrl.hostname;
