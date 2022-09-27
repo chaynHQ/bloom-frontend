dc736f258cbe0f3c82cb21e24b169d295cb4add6
swetha-chayn <swetha@chayn.co>
2022-09-27 12:31:20 +0100

    Fix bug with wrong visible courses
    5b3a60b620 / Wrong courses showing

--- config/featureFlag.ts
+++ config/featureFlag.ts
@@ -1,10 +1,10 @@
 export namespace FeatureFlag {
-  export const getDisabledCourses = () => {
+  export const getDisabledCourses: () => Set<string> = () => {
     const disabledCourses: string[] = [];
 
     const disabledCoursesString = process.env.FF_DISABLED_COURSES;
     if (disabledCoursesString) disabledCourses.push(...disabledCoursesString.split(','));
 
-    return disabledCourses;
+    return new Set<string>(disabledCourses);
   };
 }
