568220ebc971f68e6cf76b8ab61b14be74d9e09b
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-12-31 15:22:17 +0000

    Make super admin dashboard

---
+++ components/forms/CreatePartnerAdminForm.tsx
@@ -0,0 +1,157 @@
+import LoadingButton from '@mui/lab/LoadingButton';
+import { MenuItem, TextField, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { useTranslations } from 'next-intl';
+import * as React from 'react';
+import { useState } from 'react';
+import { useAddPartnerAdminMutation, useGetPartnersQuery } from '../../app/api';
+import { RootState } from '../../app/store';
+import rollbar from '../../config/rollbar';
+import {
+  CREATE_PARTNER_ADMIN_ERROR,
+  CREATE_PARTNER_ADMIN_REQUEST,
+  CREATE_PARTNER_ADMIN_SUCCESS,
+} from '../../constants/events';
+import { useTypedSelector } from '../../hooks/store';
+import { getErrorMessage } from '../../utils/errorMessage';
+import logEvent, { getEventUserData } from '../../utils/logEvent';
+
+const CreatePartnerAdminForm = () => {
+  const { partnerAdmin, user, partnerAccesses, partners } = useTypedSelector(
+    (state: RootState) => state,
+  );
+  const eventUserData = getEventUserData({ user, partnerAccesses, partnerAdmin });
+  useGetPartnersQuery(undefined);
+
+  const t = useTranslations('Admin.createPartnerAdmin');
+  const tS = useTranslations('Shared');
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [selectedPartner, setSelectedPartner] = useState<string>('');
+  const [email, setEmail] = useState<string | null>(null);
+  const [name, setName] = useState<string | null>(null);
+
+  const [formSubmitSuccess, setFormSubmitSuccess] = useState<boolean>(false);
+  const [addPartnerAdmin, { isLoading: addPartnerAdminIsLoading }] = useAddPartnerAdminMutation();
+  const [formError, setFormError] = useState<
+    | string
+    | React.ReactNodeArray
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+  >();
+
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    logEvent(CREATE_PARTNER_ADMIN_REQUEST, eventUserData);
+    if (email == null || name == null) {
+      console.log('error');
+      return;
+    }
+    const partnerAdminResponse = await addPartnerAdmin({
+      email,
+      partnerId: selectedPartner,
+      name,
+    });
+
+    if ('data' in partnerAdminResponse) {
+      logEvent(CREATE_PARTNER_ADMIN_SUCCESS, eventUserData);
+    }
+
+    if ('error' in partnerAdminResponse) {
+      const error = partnerAdminResponse.error;
+      const errorMessage = getErrorMessage(error);
+
+      logEvent(CREATE_PARTNER_ADMIN_ERROR, {
+        ...eventUserData,
+        error: errorMessage,
+      });
+      rollbar.error('Create partner admin code error', error);
+
+      setFormError(t('createPartnerAdminError') + error);
+      setLoading(false);
+      return;
+    }
+
+    setLoading(false);
+    setFormSubmitSuccess(true);
+  };
+
+  const resetForm = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setFormSubmitSuccess(false);
+    setSelectedPartner('');
+    setEmail(null);
+    setName(null);
+  };
+
+  const FormResetButton = () => (
+    <Box>
+      <Button sx={{ mt: 3 }} variant="contained" color="secondary" onClick={resetForm}>
+        {t('reset')}
+      </Button>
+    </Box>
+  );
+
+  const FormSuccess = () => (
+    <Box>
+      <Typography>{t('successDescription')}</Typography>
+      <FormResetButton />
+    </Box>
+  );
+
+  if (formSubmitSuccess) {
+    return <FormSuccess />;
+  }
+
+  return (
+    <form autoComplete="off" onSubmit={submitHandler}>
+      <TextField
+        key="select-partner"
+        fullWidth
+        select
+        label={t('partnerNameLabel')}
+        onChange={(e) => setSelectedPartner(e.target.value)}
+        variant="standard"
+        required
+        value={selectedPartner}
+      >
+        {partners.partners.map((option, index) => (
+          <MenuItem key={`partner-name-${index}`} value={option.id}>
+            {option.name}
+          </MenuItem>
+        ))}
+      </TextField>
+
+      <TextField
+        key="email-input"
+        onChange={(e) => setEmail(e.target.value)}
+        label={t('emailAddressLabel')}
+        variant="standard"
+        type="email"
+        fullWidth
+        required
+        value={email}
+      />
+      <TextField
+        key="name-input"
+        onChange={(e) => setName(e.target.value)}
+        label={t('nameLabel')}
+        variant="standard"
+        type="text"
+        fullWidth
+        required
+        value={name}
+      />
+
+      {formError && <Typography color="error.main">{formError}</Typography>}
+
+      <LoadingButton variant="contained" color="secondary" type="submit" loading={loading}>
+        {t('title')}
+      </LoadingButton>
+    </form>
+  );
+};
+
+export default CreatePartnerAdminForm;
---
+++ pages/admin/dashboard.tsx
@@ -0,0 +1,78 @@
+import { Card, CardContent, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { GetStaticPropsContext, NextPage } from 'next';
+import { useTranslations } from 'next-intl';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { RootState } from '../../app/store';
+import CreatePartnerAdminForm from '../../components/forms/CreatePartnerAdminForm';
+import AdminHeader from '../../components/layout/PartnerAdminHeader';
+import { CREATE_PARTNER_ACCESS_VIEWED } from '../../constants/events';
+import { useTypedSelector } from '../../hooks/store';
+import { rowStyle } from '../../styles/common';
+import logEvent, { getEventUserData } from '../../utils/logEvent';
+
+const containerStyle = {
+  backgroundColor: 'secondary.light',
+  ...rowStyle,
+} as const;
+
+const cardStyle = {
+  width: { xs: '100%', md: '60%' },
+} as const;
+
+const Dashboard: NextPage = () => {
+  const t = useTranslations('Admin');
+  const { partnerAdmin, user, partnerAccesses } = useTypedSelector((state: RootState) => state);
+  const eventUserData = getEventUserData({ user, partnerAccesses, partnerAdmin });
+
+  const headerProps = {
+    title: t('title'),
+  };
+
+  useEffect(() => {
+    logEvent(CREATE_PARTNER_ACCESS_VIEWED, { ...eventUserData });
+  }, []);
+
+  return (
+    <Box>
+      <Head>
+        <title>{headerProps.title}</title>
+      </Head>
+      <AdminHeader title={headerProps.title} />
+      <Container sx={containerStyle}>
+        <Card sx={cardStyle}>
+          <CardContent>
+            <Typography variant="h2" component="h2">
+              {t('createPartnerAdmin.title')}
+            </Typography>
+            <Typography
+              sx={{
+                '&:last-of-type': {
+                  marginBottom: '1em',
+                },
+              }}
+            ></Typography>
+            <CreatePartnerAdminForm />
+          </CardContent>
+        </Card>
+      </Container>
+    </Box>
+  );
+};
+
+export function getStaticProps({ locale }: GetStaticPropsContext) {
+  return {
+    props: {
+      messages: {
+        ...require(`../../messages/shared/${locale}.json`),
+        ...require(`../../messages/navigation/${locale}.json`),
+        ...require(`../../messages/partnerAdmin/${locale}.json`),
+        ...require(`../../messages/Admin/${locale}.json`),
+      },
+    },
+  };
+}
+
+export default Dashboard;
