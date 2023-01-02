c4cbb201c09e9b84ad0217ba4079c2690b477fa4
swetha-chayn <swetha@chayn.co>
2023-01-02 16:12:34 -0600

    Disable PWA temporarily
    PWA is causing errors and this change is to prevent these errors.
    As this feature does not seem to be used significantly, it will be removed at a future date.
    
    ee18f0711a / Disable PWA

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ module.exports = withPWA({
   pwa: {
     dest: 'public',
     skipWaiting: true,
-    disable: process.env.NEXT_PUBLIC_ENV === 'local' ? true : false,
+    disable: true,
   },
   async redirects() {
     return [
