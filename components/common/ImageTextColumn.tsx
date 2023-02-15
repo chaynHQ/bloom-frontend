17166808a28797903d6a2d0fdf2b39d1cdb355c5
swetha-chayn <swetha@chayn.co>
2023-02-15 17:38:30 +0000

    Add form to subscribe or unsubscribe with whatsapp
    Whether a user has an active subscription is used to determine which form to display.
    
    Note that the unsubscribe endpoint still needs to be hooked up.
    
    ecbb215932 / Build whatsapp subscribe FE

---
+++ components/common/ImageTextColumn.tsx
@@ -0,0 +1,69 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { useTranslations } from 'next-intl';
+import Image, { StaticImageData } from 'next/image';
+
+export interface ImageTextItem {
+  text: string;
+  illustrationSrc: StaticImageData;
+  illustrationAlt: string;
+}
+
+interface ImageTextGridProps {
+  items: Array<ImageTextItem>;
+  translations: string;
+}
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'left',
+} as const;
+
+const itemContainerStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  paddingX: 2,
+  paddingY: 1,
+  alignItems: 'center',
+} as const;
+
+const imageContainerStyle = {
+  position: 'relative', // needed for next/image to fill the container
+  width: 100,
+  height: 100,
+  paddingX: 5,
+  marginX: 1,
+} as const;
+
+const textStyle = {
+  marginTop: 'auto',
+  marginBottom: 'auto',
+} as const;
+
+const ImageTextColumn = (props: ImageTextGridProps) => {
+  const { items, translations } = props;
+
+  const t = useTranslations(translations);
+  const tS = useTranslations('Shared');
+
+  return (
+    <Box sx={containerStyle}>
+      {items.map((item, i) => (
+        <Box key={`item${i}`} sx={itemContainerStyle}>
+          <Box sx={imageContainerStyle}>
+            <Image
+              alt={tS(item.illustrationAlt)}
+              src={item.illustrationSrc}
+              layout="fill"
+              objectFit="contain"
+            />
+          </Box>
+          <Typography sx={textStyle}>{t(item.text)}</Typography>
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+export default ImageTextColumn;
