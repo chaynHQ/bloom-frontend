e16fa6cece1ce751d81930567eea8f37396bcfe1
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-03-30 18:13:15 +0100

    Update text for denested chat page

--- components/layout/NavigationDrawerLinks.tsx
+++ components/layout/NavigationDrawerLinks.tsx
@@ -8,13 +8,13 @@ import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { RootState } from '../../app/store';
 import {
   DRAWER_ADMIN_CLICKED,
+  DRAWER_CHAT_CLICKED,
   DRAWER_COURSES_CLICKED,
   DRAWER_IMMEDIATE_HELP_CLICKED,
   DRAWER_LOGIN_CLICKED,
   DRAWER_NOTES_CLICKED,
   DRAWER_OUR_BLOOM_TEAM_CLICKED,
   DRAWER_THERAPY_CLICKED,
-  HEADER_LOGIN_CLICKED,
 } from '../../constants/events';
 import { useTypedSelector } from '../../hooks/store';
 import logEvent, { getEventUserData } from '../../utils/logEvent';
@@ -105,6 +105,11 @@ const NavigationMenu = (props: NavigationMenuProps) => {
       // TODO bring in translations so that "notes" is translated
       if (user.token) {
         links.push({ title: t('courses'), href: '/courses', event: DRAWER_COURSES_CLICKED });
+        links.push({
+          title: t('chat'),
+          href: '/chat',
+          event: DRAWER_CHAT_CLICKED,
+        });
         links.push({
           title: t('notes'),
           href: '/subscription/whatsapp',
