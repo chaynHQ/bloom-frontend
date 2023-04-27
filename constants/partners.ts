a546cb08b257d9b2d941144f772db95c86bd0335
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-04-27 21:45:10 +0100

    Add fruitz assets

--- constants/partners.ts
+++ constants/partners.ts
@@ -2,8 +2,12 @@ import { StaticImageData } from 'next/image';
 import badooLogo from '../public/badoo_logo.svg';
 import bloomBadooLogo from '../public/bloom_badoo_logo.svg';
 import bloomBumbleLogo from '../public/bloom_bumble_logo.svg';
+import bloomFruitzLogo from '../public/bloom_fruitz_logo.svg';
+
 import bloomLogo from '../public/bloom_logo.svg';
 import bumbleLogo from '../public/bumble_logo.svg';
+import fruitzLogo from '../public/fruitz_logo.svg';
+import illustrationBloomHeadPink from '../public/illustration_bloom_head_pink.svg';
 import illustrationBloomHeadPurple from '../public/illustration_bloom_head_purple.svg';
 import illustrationBloomHeadYellow from '../public/illustration_bloom_head_yellow.svg';
 
@@ -66,16 +70,33 @@ export const badooContent: PartnerContent = {
   facebook: 'https://www.facebook.com/badoo/',
   instagram: 'https://www.instagram.com/badoo/',
   youtube: 'https://www.youtube.com/c/badoo',
-  tiktok: 'https://twitter.com/Badoo',
+  twitter: 'https://twitter.com/Badoo',
+};
+
+export const fruitzContent: PartnerContent = {
+  name: 'Fruitz',
+  logo: fruitzLogo,
+  logoAlt: 'alt.fruitzLogo',
+  partnershipLogo: bloomFruitzLogo,
+  partnershipLogoAlt: 'alt.bloomFruitzLogo',
+  bloomGirlIllustration: illustrationBloomHeadPink,
+  website: 'https://fruitz.io',
+  footerLine1: 'footer.fruitzLine1',
+  footerLine2: 'footer.fruitzLine2',
+  instagram: 'https//www.instagram.com/fruitz_app/',
+  youtube: 'https://www.youtube.com/channel/UCvfMffckjzOZtYIDeTuZq2w',
+  tiktok: '//www.tiktok.com/@fruitz_app',
 };
 
 export const getPartnerContent = (partnerName: string) => {
   const partner = partnerName.toLowerCase();
   if (partner === 'bumble') return bumbleContent;
   if (partner === 'badoo') return badooContent;
+  if (partner === 'fruitz') return fruitzContent;
+
   return publicContent;
 };
 
 export const getAllPartnersContent = () => {
-  return [bumbleContent, badooContent];
+  return [bumbleContent, badooContent, fruitzContent];
 };
