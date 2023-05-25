0cfe7d8c14f2ff46b6b4b466f9ef9356c4742665
swetha-chayn <swetha@chayn.co>
2023-05-25 19:19:38 +0100

    Refactor session progress display to new component
    This logic is taken from courses/[slug] and has been placed in the new SessionProgressDisplay component. This logic is currently also found in courses/image-based-abuse/index.ts
    
    Creating this new component means the shared logic and be removed and this new component used instead.

---
+++ components/session/SessionProgressDisplay.tsx
@@ -0,0 +1,53 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import Box from '@mui/material/Box';
+import { useEffect, useState } from 'react';
+import { Course } from '../../app/coursesSlice';
+import { RootState } from '../../app/store';
+import { PROGRESS_STATUS } from '../../constants/enums';
+import { useTypedSelector } from '../../hooks/store';
+import { StoryblokSessionJson } from '../../utils/helper-types/storyblok';
+
+interface SessionProgressDisplayProps {
+  session: StoryblokSessionJson;
+  storyblokCourseId: number;
+}
+
+export const SessionProgressDisplay = (props: SessionProgressDisplayProps) => {
+  const { session, storyblokCourseId } = props;
+
+  const [sessionProgress, setSessionProgress] = useState<PROGRESS_STATUS>(
+    PROGRESS_STATUS.NOT_STARTED,
+  );
+
+  const { courses } = useTypedSelector((state: RootState) => state);
+
+  useEffect(() => {
+    const userCourse = courses.find((course: Course) => course.storyblokId === storyblokCourseId);
+
+    if (userCourse) {
+      const matchingSession = userCourse.sessions?.find(
+        (userSession) => userSession.storyblokId === session.id,
+      );
+
+      if (matchingSession) {
+        matchingSession.completed
+          ? setSessionProgress(PROGRESS_STATUS.COMPLETED)
+          : setSessionProgress(PROGRESS_STATUS.STARTED);
+      } else {
+        setSessionProgress(PROGRESS_STATUS.NOT_STARTED);
+      }
+    }
+  }, [courses, session.id, storyblokCourseId]);
+
+  return (
+    <>
+      {sessionProgress !== PROGRESS_STATUS.NOT_STARTED && (
+        <Box mt={0.5}>
+          {sessionProgress === PROGRESS_STATUS.STARTED && <DonutLargeIcon color="error" />}
+          {sessionProgress === PROGRESS_STATUS.COMPLETED && <CheckCircleIcon color="error" />}
+        </Box>
+      )}
+    </>
+  );
+};
