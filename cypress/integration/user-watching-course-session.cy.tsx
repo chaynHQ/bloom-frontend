0e57fbbe01f62bc6c89cea8778a50ffd49576009
Zahra Ali <zahraali@Zahras-MacBook-Pro.local>
2023-06-19 09:15:38 +0100

    new cypress test: user can do a course

---
+++ cypress/integration/user-watching-course-session.cy.tsx
@@ -0,0 +1,46 @@
+describe('A logged in user should be able to watch a course session and complete it', () => {
+  const newUserEmail = `cypresstestemail+${Date.now()}@chayn.co`;
+  const password = 'testpassword';
+
+  before(() => {
+    cy.cleanUpTestState();
+
+    cy.createUser({
+      //create test user
+      emailInput: newUserEmail,
+      passwordInput: password,
+    });
+  });
+
+  it('Should go to the courses page and select a course', () => {
+    cy.logInWithEmailAndPassword(newUserEmail, password); //log in to test user
+
+    cy.visit('/courses'); //navigate to courses
+
+    cy.get('a[href*="healing-from-sexual-trauma"]', {
+      timeout: 4000,
+    })
+      .first()
+      .click(); //click on a course when link load
+
+    // cy.getIframeBody().find('button').click(); iframe isnt working at the moment
+
+    cy.get('a[href*="what-is-sexual-trauma"]', {
+      timeout: 4000,
+    })
+      .first()
+      .click(); //click on a session when link loads
+
+    cy.get('h3').contains('Activity').click(); //open activities
+
+    cy.get('h3').contains('Bonus content').click(); //open bonus content
+
+    cy.get('button').contains('Session complete').click(); //mark course as complete
+
+    cy.deleteUserByEmail(newUserEmail); //delete test user
+  });
+
+  after(() => {
+    cy.logout();
+  });
+});
