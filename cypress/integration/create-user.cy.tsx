672039ca82ecc16084f7c0284fe89c706cd54d1f
Zahra Ali <zahraali@Zahras-MBP.lan>
2023-07-25 10:25:34 +0100

    create user test to wait longer in timeout. Testing to see if fails are because of time taken

--- cypress/integration/create-user.cy.tsx
+++ cypress/integration/create-user.cy.tsx
@@ -9,18 +9,16 @@ describe('Create User', () => {
   it('User should be able to create user', () => {
     // Start from the home page
     cy.visit('/');
-    cy.wait(2000);
-    cy.get('h2', { timeout: 2000 }).contains('Get started').should('exist');
+    cy.get('h2', { timeout: 5000 }).contains('Get started').should('exist');
     // TODO - workout why #primary-get-started-button works locallly and headless but not in github actions
-    cy.get('a[href="/auth/register"]', { timeout: 5000 }).first().click({ force: true });
-    cy.wait(2000);
+    cy.get('a[href="/auth/register"]').first().click({ force: true });
     cy.get('h2', { timeout: 5000 }).should('contain', 'Create account');
     cy.get('#name', { timeout: 5000 }).type('Cypress test');
     cy.get('#email', { timeout: 5000 }).type(username);
     cy.get('#password', { timeout: 5000 }).type('testpassword');
     cy.get('button[type="submit"]').contains('Create account').click();
     cy.wait(3000);
-    cy.get('h2', { timeout: 3000 }).should('contain', 'Help us understand');
+    cy.get('h2', { timeout: 5000 }).should('contain', 'Help us understand');
   });
   after(() => {
     cy.logout();
