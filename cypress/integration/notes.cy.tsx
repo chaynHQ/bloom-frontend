2af0445ff6c02202b05b4a0ac0a51ff0e969ba9c
Zahra Ali <zahraali@Zahras-MacBook-Pro.local>
2023-07-28 15:01:01 +0100

    new cypress test for notes

---
+++ cypress/integration/notes.cy.tsx
@@ -0,0 +1,22 @@
+describe('A logged in user should be able to subscribe to notes from bloom', () => {
+  before(() => {
+    cy.cleanUpTestState();
+    cy.logInWithEmailAndPassword(Cypress.env('public_email'), Cypress.env('public_password'));
+  });
+
+  it('Should go to the notes page and try to subscribe using an incorrect number', () => {
+    cy.get(`[qa-id=secondary-nav-notes-button]`).should('exist').click(); //navigate to notes
+
+    cy.get('h2').contains('Subscribe to Notes from Bloom').should('exist'); //check subscribe to notes form exists
+
+    cy.get('input[type="tel"]').should('exist').type('0101'); //type invalid number
+
+    cy.get('button[type="submit"]').contains('Subscribe').click(); //submit form
+
+    cy.get('p', { timeout: 3000 }).should('contain', 'Your phone number appears to be invalid'); //check the form submission fails
+  });
+
+  after(() => {
+    cy.logout();
+  });
+});
