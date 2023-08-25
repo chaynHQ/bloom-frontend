d2209e93af25dd7a95b50180b28fafd25a4fdcc3
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-08-25 15:08:48 +0200

    Add cypress tests for session pages auth redirect

--- cypress/integration/auth-redirect.cy.tsx
+++ cypress/integration/auth-redirect.cy.tsx
@@ -2,10 +2,21 @@ describe('Auth redirect', () => {
   before(() => {
     cy.cleanUpTestState();
   });
-  it('User visits a page with an auth guard and should be redirected', () => {
+  it('User visits the courses page and should be redirected with correct url', () => {
     cy.visit('/courses');
     cy.wait(2000);
     cy.get('h2', { timeout: 5000 }).should('contain', 'Welcome back');
     cy.url().should('include', 'return_url=%2Fcourses');
+  });
+  it('User visits a session page with an auth guard and should be redirected', () => {
+    cy.visit(
+      '/courses/image-based-abuse-and-rebuilding-ourselves/the-social-context-of-image-based-abuse-and-victim-blaming',
+    );
+    cy.wait(2000);
+    cy.get('h2', { timeout: 5000 }).should('contain', 'Welcome back');
+    cy.url().should(
+      'include',
+      'return_url=%2Fcourses%2Fimage-based-abuse-and-rebuilding-ourselves%2Fthe-social-context-of-image-based-abuse-and-victim-blaming',
+    );
   });
 });
