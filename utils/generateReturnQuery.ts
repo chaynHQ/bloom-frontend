064087d1cf99308e124f96142f0260d3a3ad6785
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-08-25 11:20:19 +0200

    Remove redirect from sessions to courses

--- utils/generateReturnQuery.ts
+++ utils/generateReturnQuery.ts
@@ -1,7 +1,5 @@
 export default function generateReturnUrlQuery(pathname: string) {
-  return pathname.indexOf('/courses/') > -1
-    ? `?${generateReturnUrlParam('/courses')}`
-    : `?${generateReturnUrlParam(pathname)}`;
+  return `?${generateReturnUrlParam(pathname)}`;
 }
 
 export const generateReturnUrlParam = (path: string) => `return_url=${encodeURIComponent(path)}`;
