6386f9816d5fb26db35b77687205fd389d904579
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-08-31 17:38:05 +0200

    Add env toggle for simplybook Additional field id

--- config/simplybook.ts
+++ config/simplybook.ts
@@ -1,5 +1,11 @@
 import { User } from '../app/userSlice';
 
+// Simplybook hardcodes additional field IDs
+const userIdFieldId =
+  process.env.NEXT_PUBLIC_ENV === 'production'
+    ? '86a541b6d059de75eaba4e18a288cd24'
+    : 'b3b2455c79e69e6baf6e8c1fcf34b691';
+
 export const getSimplybookWidgetConfig = (user?: User) => {
   return {
     widget_type: 'iframe',
@@ -34,8 +40,8 @@ export const getSimplybookWidgetConfig = (user?: User) => {
             email: user.email,
           },
           fields: {
-            b3b2455c79e69e6baf6e8c1fcf34b691: user.id,
-          }
+            [userIdFieldId]: user.id,
+          },
         },
       }),
     },
