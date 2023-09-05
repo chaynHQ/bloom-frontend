47dc61b60989afaac185809ba9e9b926bd6940a4
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-09-05 14:07:40 +0100

    Remove feature flag for grounding and activities

--- config/featureFlag.ts
+++ config/featureFlag.ts
@@ -11,9 +11,6 @@ export namespace FeatureFlag {
   export const isUserResearchBannerEnabled = () => {
     return process.env.NEXT_PUBLIC_FF_USER_RESEARCH_BANNER?.toLowerCase() === 'true';
   };
-  export const isGroundingAndActivitiesEnabled = () => {
-    return process.env.NEXT_PUBLIC_ENV !== 'production';
-  };
   export const isGermanEnabled = () => {
     return process.env.NEXT_PUBLIC_ENV !== 'production';
   };
