f5386628d1c2d86cd486cf4487b93c8cfec54986
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-11-14 18:26:39 +0000

    fix: http-https redirect fix 2

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,10 @@ import { ENVIRONMENT } from './constants/enums';
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   const environments = [ENVIRONMENT.STAGING, ENVIRONMENT.PRODUCTION];
   const currentEnv = process.env.NEXT_PUBLIC_ENV as ENVIRONMENT;
-
   if (environments.includes(currentEnv) && req.headers.get('x-forwarded-proto') !== 'https') {
-    const hostname = req.headers.get('host') || req.nextUrl.hostname;
+    const hostname = req.headers.get('host') || req.nextUrl.host;
     const locale = req.nextUrl.locale === 'en' ? '' : `/${req.nextUrl.locale}`
-    return NextResponse.redirect(`https://${hostname}${locale}${req.nextUrl.pathname}`, 301);
+    return NextResponse.redirect(`https://${hostname}${locale}${req.nextUrl.pathname}`, 307);
   }
   return NextResponse.next();
 }
