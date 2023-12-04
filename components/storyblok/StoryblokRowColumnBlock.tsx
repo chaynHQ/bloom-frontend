3860943ac2bf043ae48162e4d9a35a2dc6c7c959
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-12-04 16:57:01 +0000

    refactor: storyblok row and column components

--- components/storyblok/StoryblokRowColumnBlock.tsx
+++ components/storyblok/StoryblokRowColumnBlock.tsx
@@ -1,7 +1,7 @@
-import { Box } from '@mui/system';
 import { render } from 'storyblok-rich-text-react-renderer';
-import { richtextContentStyle, rowStyle } from '../../styles/common';
 import { RichTextOptions } from '../../utils/richText';
+import Column from '../common/Column';
+import Row from '../common/Row';
 import { StoryblokColumn } from './StoryblokTypes';
 
 /**
@@ -27,63 +27,20 @@ const StoryblokRowColumnBlock = (props: StoryblokRowColumnBlockProps) => {
 
   if (!columns) return <></>;
 
-  const rowStyles = {
-    width: '100%',
-    gap: { xs: 3, sm: 8 / columns.length, md: 10 / columns.length, lg: 16 / columns.length },
-    ...rowStyle,
-    textAlign:
-      horizontal_alignment === 'center'
-        ? 'center'
-        : horizontal_alignment === 'right'
-        ? 'right'
-        : 'left',
-    ...(horizontal_alignment && {
-      justifyContent:
-        horizontal_alignment === 'center'
-          ? 'center'
-          : horizontal_alignment === 'right'
-          ? 'flex-end'
-          : 'flex-start',
-    }),
-    ...(vertical_alignment && {
-      alignItems:
-        vertical_alignment === 'center'
-          ? 'center'
-          : vertical_alignment === 'bottom'
-          ? 'flex-end'
-          : 'flex-start',
-    }),
-    ...richtextContentStyle,
-  } as const;
-
   return (
-    <Box sx={rowStyles}>
+    <Row
+      numberOfColumns={columns.length}
+      verticalAlignment={vertical_alignment}
+      horizontalAlignment={horizontal_alignment}
+    >
       {columns.map((column: any, index: number) => {
-        const columnStyles = {
-          width:
-            column.width === 'extra-small'
-              ? {
-                  xs: `20%`,
-                  md: '5%',
-                }
-              : column.width === 'small'
-              ? { xs: '100%', md: '20%' }
-              : column.width === 'medium'
-              ? { xs: '100%', md: '40%' }
-              : column.width === 'large'
-              ? { xs: '100%', md: '60%' }
-              : column.width === 'extra-large'
-              ? { xs: '100%', md: '80%' }
-              : { xs: `100%`, md: 'auto' },
-          ...(!column.width && { flex: { md: 1 } }),
-        };
         return (
-          <Box sx={columnStyles} key={`row_column_${index}_${Math.random()*100}`}>
+          <Column width={column.width} key={`row_column_${index}_${Math.random() * 100}`}>
             {render(column.content, RichTextOptions)}
-          </Box>
+          </Column>
         );
       })}
-    </Box>
+    </Row>
   );
 };
 
