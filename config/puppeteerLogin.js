e2c35042002259911859a90a59cce4b1f41808c8
annarhughes <annaraehughes@live.co.uk>
2023-12-05 15:41:53 +0000

    fix type

--- config/puppeteerLogin.js
+++ config/puppeteerLogin.js
@@ -7,8 +7,8 @@ module.exports = async (browser, context) => {
   const page = await browser.newPage();
   await page.goto('https://bloom-frontend-git-develop-chaynhq.vercel.app/auth/login');
   await page.waitForSelector('input[type="email"]', { visible: true });
-  await page.type('input[name=email]', process.env.USER_EMAIL);
-  await page.type('input[name=password]', process.env.USER_PASSWORD);
+  await page.type('input[type=email]', process.env.USER_EMAIL);
+  await page.type('input[type=password]', process.env.USER_PASSWORD);
   await Promise.all([page.click('input[type=submit]'), page.waitForNavigation()]);
   // close session for next run
   await page.close();
