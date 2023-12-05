f92ce0f9606cadf7e73d8f5a2b96f0c22ed33b9a
annarhughes <annaraehughes@live.co.uk>
2023-12-05 16:21:43 +0000

    console log

--- config/puppeteerLogin.js
+++ config/puppeteerLogin.js
@@ -7,9 +7,12 @@ module.exports = async (browser, context) => {
   const page = await browser.newPage();
   await page.goto('https://bloom-frontend-git-develop-chaynhq.vercel.app/auth/login');
   await page.waitForSelector('input[type="email"]', { visible: true });
+  console.log('email is ', process.env.USER_EMAIL);
   await page.type('input[type="email"]', process.env.USER_EMAIL);
   await page.type('input[type="password"]', process.env.USER_PASSWORD);
+  console.log(page.$('input[type="email"]'));
   await Promise.all([page.waitForNavigation(), page.click('[type="submit"]')]);
+  console.log('completed');
   // close session for next run
   await page.close();
 };
