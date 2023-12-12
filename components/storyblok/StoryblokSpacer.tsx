b5879915d740595c4f8bf3dcfa46bccf51ae4f20
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-12-12 09:43:45 +0000

    fix: minor styling tweaks for carousel and spacer

--- components/storyblok/StoryblokSpacer.tsx
+++ components/storyblok/StoryblokSpacer.tsx
@@ -1,11 +1,17 @@
+import { useTheme } from '@mui/material';
 import { Box } from '@mui/system';
 interface StoryblokSpacerProps {
   size: string;
 }
 
 const StoryblokSpacer = ({ size = 'small' }: StoryblokSpacerProps) => {
+  const theme = useTheme();
   const style = {
-    ...(size === 'small' ? { height: 1 } : size === 'medium' ? { height: 2 } : { height: 3 }),
+    ...(size === 'small'
+      ? { height: theme.spacing(1) }
+      : size === 'medium'
+      ? { height: theme.spacing(2) }
+      : { height: theme.spacing(3) }),
   };
   return <Box sx={style} />;
 };
