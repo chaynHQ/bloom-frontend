9221846beacb19ca0f8291a19fc1605f3b0aa061
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2023-12-14 10:22:22 +0000

    fix: activities test failing due to to auth rerender

--- cypress/integration/activities.cy.tsx
+++ cypress/integration/activities.cy.tsx
@@ -5,7 +5,11 @@ describe('A logged in user should be able to navigate to activities and do an ex
   });
 
   it('Should go to the activities page and click on an exercise', () => {
-    cy.get(`[qa-id=secondary-nav-activities-button]`, {timeout: 10000}).should('exist').click().click(); //navigate to activities - double clicking just in case
+    cy.get(`[qa-id=secondary-nav-activities-button]`, { timeout: 10000 })
+      .should('exist')
+      .click()
+      .click(); //navigate to activities - double clicking just in case
+    cy.wait(2000); // wait to ensure the page has rendered and the auth checks have resolved
     // Default timeout is 4 seconds so extended to 8 to avoid racy tests
     cy.get('h3', { timeout: 8000 }).contains('Thought diaries').should('exist').click(); //check click first  exercise exists and open it
 
