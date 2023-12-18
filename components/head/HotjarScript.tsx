17de2dd68639c1b3245851c56fc2056cefac797f
annarhughes <annaraehughes@live.co.uk>
2023-12-18 13:28:41 +0000

    static id

--- components/head/HotjarScript.tsx
+++ components/head/HotjarScript.tsx
@@ -9,7 +9,7 @@ const HotjarScript = () => {
         __html: `
           (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
+            h._hjSettings={hjid:3687357,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
