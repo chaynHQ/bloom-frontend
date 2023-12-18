ba41803ee955c5cdd32131f6404aafbc4dbf98de
annarhughes <annaraehughes@live.co.uk>
2023-12-18 14:34:19 +0000

    add env var

--- components/head/HotjarScript.tsx
+++ components/head/HotjarScript.tsx
@@ -5,20 +5,19 @@ const HotjarScript = () => {
     <Script
       id="hotjar"
       strategy="afterInteractive"
-      onLoad={(e) => console.log('script loaded')}
-      onError={(e) => console.log('script error:', e)}
-    >
-      {`
-    		(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3687357,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-			`}
-    </Script>
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `,
+      }}
+    />
   );
 };
 
