ce068a60ca48bf3b6b81b1c485eb986449bfd3e4
annarhughes <annaraehughes@live.co.uk>
2023-12-20 21:24:37 +0000

    fix email

--- config/puppeteerLogin.js
+++ config/puppeteerLogin.js
@@ -2,7 +2,7 @@ async function login(page) {
   await page.goto('https://bloom-frontend-git-develop-chaynhq.vercel.app/auth/login');
   await page.waitForSelector('input[type="email"]', { visible: true });
 
-  await page.type('input[id="test"]', process.env.USER_EMAIL);
+  await page.type('input[id="email"]', process.env.USER_EMAIL);
   await page.type('input[id="password"]', process.env.USER_PASSWORD);
   await page.click('[type="submit"]');
   await page.waitForSelector('input[type="email"]', { visible: false });
