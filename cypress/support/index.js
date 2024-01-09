77552c0449bc50986f420ea54ef8a67b157e8548
Anna Hughes <annaraehughes@live.co.uk>
2024-01-09 14:31:28 +0000

    Run cypress before all only once (#806)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,17 +1,6 @@
 // index.js
 const customCommands = require('./commands.js');
 
-after(() => {
-  //Delete all cypress test users before cypress test suite runs
-  cy.logInWithEmailAndPassword(
-    Cypress.env('super_admin_email'),
-    Cypress.env('super_admin_password'),
-  );
-  cy.deleteCypressAccessCodes();
-  cy.deleteAllCypressUsers();
-  cy.logout();
-});
-
 module.exports = {
   commands: customCommands,
 };
