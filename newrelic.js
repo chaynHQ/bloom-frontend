4bcce256d82ef7229da4613aab91f09b5a0e2da1
annarhughes <annaraehughes@live.co.uk>
2024-01-10 12:21:20 +0000

    add newrelic config file

---
+++ newrelic.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const { loadEnvConfig } = require('@next/env');
+loadEnvConfig(process.cwd());
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config/default.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  app_name: process.env.NEW_RELIC_APP_NAME,
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
+  /**
+   * This setting controls distributed tracing.
+   * Distributed tracing lets you see the path that a request takes through your
+   * distributed system. Enabling distributed tracing changes the behavior of some
+   * New Relic features, so carefully consult the transition guide before you enable
+   * this feature: https://docs.newrelic.com/docs/transition-guide-distributed-tracing
+   * Default is true.
+   */
+  distributed_tracing: {
+    /**
+     * Enables/disables distributed tracing.
+     *
+     * @env NEW_RELIC_DISTRIBUTED_TRACING_ENABLED
+     */
+    enabled: true,
+  },
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info',
+  },
+  application_logging: {
+    forwarding: {
+      enabled: true,
+    },
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*',
+    ],
+  },
+};
