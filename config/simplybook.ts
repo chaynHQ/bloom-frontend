60ad90eb1fce872b2ea3c7c4cff1672daf67bbb0
Anna Hughes <annaraehughes@live.co.uk>
2024-01-16 17:48:56 +0000

    Fix simplybook user_id custom field (#822)

--- config/simplybook.ts
+++ config/simplybook.ts
@@ -1,11 +1,5 @@
 import { User } from '../app/userSlice';
 
-// Simplybook hardcodes additional field IDs
-const userIdFieldId =
-  process.env.NEXT_PUBLIC_ENV === 'production'
-    ? '86a541b6d059de75eaba4e18a288cd24'
-    : 'b3b2455c79e69e6baf6e8c1fcf34b691';
-
 export const getSimplybookWidgetConfig = (user?: User) => {
   return {
     widget_type: 'iframe',
@@ -40,7 +34,7 @@ export const getSimplybookWidgetConfig = (user?: User) => {
             email: user.email,
           },
           fields: {
-            [userIdFieldId]: user.id,
+            data_field_1: user.id,
           },
         },
       }),
