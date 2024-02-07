cb878031a5c2bacb7f2822ea397ada1494795637
annarhughes <annaraehughes@live.co.uk>
2024-02-07 18:07:33 +0000

    back button

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,9 @@
-import { Box, Container, Typography } from '@mui/material';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { Box, Container, IconButton, Typography } from '@mui/material';
 import { ISbRichtext } from '@storyblok/react';
 import { useTranslations } from 'next-intl';
 import Image, { StaticImageData } from 'next/legacy/image';
+import { useRouter } from 'next/router';
 import { JSXElementConstructor, ReactElement, ReactNodeArray } from 'react';
 import { render } from 'storyblok-rich-text-react-renderer';
 import { PROGRESS_STATUS } from '../../constants/enums';
@@ -25,11 +27,12 @@ export interface HeaderProps {
   cta?: any;
 }
 
-const headerContainerStyles = {
+const headerContainerStyle = {
   ...rowStyle,
   alignItems: 'center',
   minHeight: { xs: 220, lg: 360 },
   paddingBottom: { xs: '3rem !important', sm: '4rem !important' },
+  paddingTop: { xs: '0', md: '5rem ' },
   gap: '30px',
   background: {
     xs: 'linear-gradient(180deg, #F3D6D8 53.12%, #FFEAE1 100%)',
@@ -55,6 +58,8 @@ const textContainerStyle = {
   justifyContent: 'space-between',
   width: { xs: '100%', sm: 'auto' },
   maxWidth: { xs: '100%', sm: '55%', md: '65%' },
+  pl: { xs: 3, sm: 0 },
+  pr: { xs: 6, sm: 0 },
 } as const;
 
 const childrenContentStyle = {
@@ -63,6 +68,20 @@ const childrenContentStyle = {
 
 const textContentStyle = {
   marginTop: 'auto',
+} as const;
+
+const backButtonStyle = {
+  display: { md: 'none' },
+  width: '2.5rem',
+  marginLeft: '-0.675rem',
+  marginY: { xs: 1.5, sm: 2 },
+  paddingRight: '1rem',
+} as const;
+
+const backIconStyle = {
+  height: '1.75rem',
+  width: '1.75rem',
+  color: 'primary.dark',
 } as const;
 
 const Header = (props: HeaderProps) => {
@@ -77,13 +96,21 @@ const Header = (props: HeaderProps) => {
     cta,
   } = props;
 
+  const router = useRouter();
   const tS = useTranslations('Shared');
   const imageAltText = translatedImageAlt ? translatedImageAlt : imageAlt ? tS(imageAlt) : '';
 
   return (
-    <Container sx={headerContainerStyles}>
+    <Container sx={headerContainerStyle}>
       <UserResearchBanner />
       <Box sx={textContainerStyle}>
+        <IconButton
+          sx={backButtonStyle}
+          onClick={() => router.back()}
+          aria-label={tS('navigateBack')}
+        >
+          <KeyboardArrowLeftIcon sx={backIconStyle} />
+        </IconButton>
         {children && <Box sx={childrenContentStyle}>{children}</Box>}
         <Box sx={textContentStyle}>
           <Typography variant="h1" component="h1">
