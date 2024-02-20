221fe48ebd519d3eadc08c0e33c947b8abfa894e
Anna Hughes <annaraehughes@live.co.uk>
2024-02-20 17:52:22 +0000

    fix: cypress run cleanup once after all (#857)

--- cypress.config.ts
+++ cypress.config.ts
@@ -17,10 +17,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/config/plugins')(on, config);
     },
-    specPattern: [
-      './cypress/integration/_beforeAll.cy.tsx',
-      'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    ],
+    specPattern: ['cypress/integration/**/*.cy.{js,jsx,ts,tsx}'],
     baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/index.js',
   },
