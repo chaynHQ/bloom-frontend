1558b25afdbd40a9f81eb8973ded95dfd461ccad
annarhughes <annaraehughes@live.co.uk>
2024-02-27 21:40:32 +0000

    improved gender sheets data

---
+++ constants/gender.ts
@@ -0,0 +1,38 @@
+export const genderOptions = [
+  {
+    englishLabel: 'woman',
+    translationLabel: 'woman',
+  },
+  {
+    englishLabel: 'man',
+    translationLabel: 'man',
+  },
+  {
+    englishLabel: 'non binary',
+    translationLabel: 'nonBinary',
+  },
+  {
+    englishLabel: 'transgender',
+    translationLabel: 'transgender',
+  },
+  {
+    englishLabel: 'intersex',
+    translationLabel: 'intersex',
+  },
+  {
+    englishLabel: 'two-spirit',
+    translationLabel: 'twoSpirit',
+  },
+  {
+    englishLabel: 'gender non conforming',
+    translationLabel: 'genderNonConforming',
+  },
+  {
+    englishLabel: 'other',
+    translationLabel: 'other',
+  },
+  {
+    englishLabel: 'prefer not to answer',
+    translationLabel: 'preferNotToAnswer',
+  },
+];
