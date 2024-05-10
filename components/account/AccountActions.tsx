ef70f98374a048328ace316b72c866193ccf05c3
Anmol <ainsa2279@gmail.com>
2024-05-10 15:16:55 +0530

    Add: UI  for Account Deletion
    Added Caution modal for account deletion

--- components/account/AccountActions.tsx
+++ components/account/AccountActions.tsx
@@ -1,6 +1,8 @@
 import { Typography, CardContent, Card, Button } from "@mui/material"
-import { LoadingButton } from "@mui/lab";
+import Link from "../common/Link";
 import { useTranslations } from "next-intl"
+import DeleteAccountModal from "./DeleteAccountModal";
+import { useState } from "react";
 
 const formCardStyle = {
   width: { xs: '100%', sm: '70%', md: '45%' },
@@ -8,35 +10,39 @@ const formCardStyle = {
 } as const;
 
 const AccountActions = () => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+  const onDeleteModalClose = () => setIsDeleteModalOpen(false)
+  const onDeleteModalOpen = () => setIsDeleteModalOpen(true)
   const t = useTranslations('Account.accountSettings');
   return (
-    <Card sx={formCardStyle} >
-      <CardContent>
-        <Typography variant="h2" component="h2">
-          {t('actions.title')}
-        </Typography>
-        <Typography fontSize="1rem !important">
-          {t('actions.desc')}
-        </Typography>
-        <div style={{ display: "inline-flex", width: "100%", flexWrap: "wrap" }}>
-          <LoadingButton
-            sx={{ mt: 2, mr: 1.5, }}
-            variant="contained"
-            fullWidth
-            color="secondary"
-            type="submit"
-          >
-            {t('actions.button.resetPass')}
-          </LoadingButton>
-          <Button variant='outlined' color='error'
-            fullWidth
-            sx={{ mt: 2, mr: 1.5, }}
-          >
-            Delete Account
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+    <>
+      <Card sx={formCardStyle} >
+        <CardContent>
+          <Typography variant="h2" component="h2">
+            {t('actions.title')}
+          </Typography>
+          <Typography fontSize="1rem !important">
+            {t('actions.desc')}
+          </Typography>
+          <div style={{ display: "inline-flex", width: "100%", flexWrap: "wrap" }}>
+            <Button
+              sx={{ mt: 2, mr: 1.5, }}
+              component={Link}
+              href='/auth/reset-password'
+              variant="contained"
+              fullWidth
+              color="secondary"
+            >
+              {t('actions.button.resetPass')}
+            </Button>
+            <Button variant='outlined' color='error' fullWidth onClick={onDeleteModalOpen} sx={{ mt: 2, mr: 1.5, }} >
+              {t('actions.button.delAcc')}
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+      <DeleteAccountModal open={isDeleteModalOpen} onClose={onDeleteModalClose} />
+    </>
   )
 }
 
