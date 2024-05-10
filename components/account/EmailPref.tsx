062895c6bdd7d5590aae63ccbd9c78f356369ef1
Anmol <ainsa2279@gmail.com>
2024-05-10 15:12:58 +0530

    Add: Email Prefs Update func
    Added Functionality to update email preferences in account settings

--- components/account/EmailPref.tsx
+++ components/account/EmailPref.tsx
@@ -1,7 +1,10 @@
+import { useState, useCallback } from "react";
 import { Typography, CardContent, Card, Box } from "@mui/material"
 import { Checkbox, FormControl, FormControlLabel, } from '@mui/material';
 import { LoadingButton } from "@mui/lab";
 import { useTranslations } from "next-intl"
+import { useUpdateUserMutation } from "../../app/api";
+import { useTypedSelector } from "../../hooks/store";
 
 const formCardStyle = {
   width: { xs: '100%', sm: '70%', md: '45%' },
@@ -10,47 +13,81 @@ const formCardStyle = {
 } as const;
 
 const EmailPref = () => {
+  const [updateUser, { isLoading }] = useUpdateUserMutation()
   const t = useTranslations('Account.accountSettings.emailPref');
+
+  const cPerms = useTypedSelector(state => state.user.contactPermission)
+  const sPerms = useTypedSelector(state => state.user.serviceEmailsPermission)
+
+  console.log({ cPerms, sPerms })
+  //
+  // {
+  //   "name": "string",
+  //   "contactPermission": true,
+  //   "serviceEmailsPermission": true
+  // }
+
+  const onSubmit = useCallback(async (ev: React.FormEvent<HTMLFormElement>) => {
+    const formData = new FormData(ev.currentTarget)
+    ev.preventDefault()
+
+    const contactPermission = formData.get("cPerms") === "on"
+    const serviceEmailsPermission = formData.get("sPerms") === "on"
+    const payload = {
+      contactPermission, serviceEmailsPermission
+    }
+    updateUser(payload)
+  }, [updateUser])
+
   return (
     <Card sx={formCardStyle}>
       <CardContent>
-        <Typography variant="h2" component="h2">
-          {t('title')}
-        </Typography>
-        <Typography fontSize="1rem !important">
-          {t('desc')}
-        </Typography>
-        <Box >
-          <FormControl sx={{ marginY: 3, display: "flex", flexDirection: "column", gap: 2, }} >
-            <FormControlLabel
-              label={t('checkbox.emailOnChange')}
-              control={
-                <Checkbox
-                  aria-label={t('contactPermissionLabel')}
-                // onChange={(e) => setContactPermissionInput(e.target.value === 'true')}
-                />
-              }
-            />
-            <FormControlLabel
-              label={t('checkbox.emailOnCourse')}
-              control={
-                <Checkbox
-                  aria-label={t('contactPermissionLabel')}
-                // onChange={(e) => setContactPermissionInput(e.target.value === 'true')}
-                />
-              }
-            />
-          </FormControl>
-        </Box>
-        <LoadingButton
-          sx={{ mt: 2, mr: 1.5, }}
-          variant="contained"
-          fullWidth
-          color="secondary"
-          type="submit"
-        >
-          {t('button.savePref')}
-        </LoadingButton>
+        <form onSubmit={onSubmit}>
+          <Typography variant="h2" component="h2">
+            {t('title')}
+          </Typography>
+          <Typography fontSize="1rem !important">
+            {t('desc')}
+          </Typography>
+          <Box >
+            <FormControl sx={{ marginY: 3, display: "flex", flexDirection: "column", gap: 2, }} >
+              <FormControlLabel
+                label={t('checkbox.emailOnChange')}
+                control={
+                  <Checkbox
+                    name="cPerms"
+                    aria-label={t('checkbox.emailOnChange')}
+                    defaultChecked={cPerms}
+                  // checked={contactPerms}
+                  // onClick={() => setContactPerms(curr => !curr)}
+                  />
+                }
+              />
+              <FormControlLabel
+                label={t('checkbox.emailOnCourse')}
+                control={
+                  <Checkbox
+                    name="sPerms"
+                    aria-label={t('checkbox.emailOnCourse')}
+                    defaultChecked={sPerms}
+                  // checked={emailPerms}
+                  // onChange={() => setEmailPerms(curr => !curr)}
+                  />
+                }
+              />
+            </FormControl>
+          </Box>
+          <LoadingButton
+            sx={{ mt: 2, mr: 1.5, }}
+            variant="contained"
+            fullWidth
+            loading={isLoading}
+            color="secondary"
+            type="submit"
+          >
+            {t('button.savePref')}
+          </LoadingButton>
+        </form>
       </CardContent>
     </Card >
   )
