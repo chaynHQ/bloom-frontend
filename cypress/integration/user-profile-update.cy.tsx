1fc3ef39f3b74edca36e21c9bbc49e7cc1d35aa7
Anmol <ainsa2279@gmail.com>
2024-05-12 14:30:19 +0530

    Tests: fix testing code to match with functionality
    update user-profile-update test to make sure that input is disabled

--- cypress/integration/user-profile-update.cy.tsx
+++ cypress/integration/user-profile-update.cy.tsx
@@ -1,28 +1,14 @@
-describe('User should be able to update his profile', () => {
-  const publicEmail = Cypress.env("public_email")
-
-  const date = Date.now()
-  const tempEmail = `cypresstestemail+${date}@chayn.co`;
-  const tempName = `cypresstestname+${date}`;
+describe('User should be able to see his profile data in settings', () => {
+  const publicEmail = Cypress.env('public_email')
 
   before(() => {
     cy.cleanUpTestState();
     cy.logInWithEmailAndPassword(publicEmail, Cypress.env('public_password'));
   });
 
-  it('Navigate to the account settings page and update profile details', () => {
+  it('Navigate to the account settings page', () => {
     cy.visit('/account/settings');
-    cy.get('#name', { timeout: 10000 }).clear().type(tempName);
-    cy.get('#email', { timeout: 10000 }).clear().type(tempEmail);
-    cy.get('button[type="submit"]').contains('Save Profile').click();
-    cy.wait(3000);
-  });
-
-  it('Reset the email back to public one', () => {
-    cy.visit('/account/settings');
-    cy.get('#name', { timeout: 10000 }).clear().type("New Name");
-    cy.get('#email', { timeout: 10000 }).clear().type(publicEmail);
-    cy.get('button[type="submit"]').contains('Save Profile').click();
+    cy.get('#email', { timeout: 10000 }).should('be.disabled')
     cy.wait(3000);
   });
 
