6930d6b3400100ca9e6260b2e520acb49f4f82a4
Anmol <ainsa2279@gmail.com>
2024-05-12 14:14:47 +0530

    Removed: profile update functionality
    remove profile update feature and disabled inputs with pre-populated data as mention in Chat

--- components/forms/ProfileDetailsForm.tsx
+++ components/forms/ProfileDetailsForm.tsx
@@ -1,71 +1,25 @@
-import { Box, TextField, Container, Typography } from '@mui/material';
+import { Box, TextField } from '@mui/material';
 import { useTranslations } from 'next-intl';
 import * as React from 'react';
-import { useState } from 'react';
 import { useTypedSelector } from '../../hooks/store';
-import { LoadingButton } from '@mui/lab';
-import { getAuth, verifyBeforeUpdateEmail } from 'firebase/auth';
-import { useUpdateUserMutation } from '../../app/api';
-import { ReactElement, JSXElementConstructor, ReactNode } from 'react'
 
 const containerStyle = {
   marginY: 3,
 } as const;
 
-
-const onEmailUpdate = (newEmail: string) => {
-  const auth = getAuth()
-  const currUser = auth.currentUser
-
-  if (currUser === null) {
-    return
-  }
-
-  verifyBeforeUpdateEmail(currUser, newEmail).then((res) => {
-    console.log(res)
-  }).catch(err => {
-    console.log(err)
-  })
-}
-
-type Err = string | ReactElement<any, string | JSXElementConstructor<any>> | ReactNode | null
-
 const ProfileDetailsForm = () => {
-  const [updateUser, { isLoading }] = useUpdateUserMutation()
-  const [error, setError] = useState<Err>(null);
-
   const name = useTypedSelector(state => state.user.name)
   const email = useTypedSelector(state => state.user.email)
 
-  const [nameInput, setNameInput] = useState<string>(name ?? '');
-  const [emailInput, setEmailInput] = useState<string>(email ?? '');
-
   const tA = useTranslations('Account.accountSettings.form');
-
-  const onUserUpdate = ({ name, email }: { name?: string, email?: string }) => {
-    try {
-      updateUser({ name, email })
-      if (email) {
-        onEmailUpdate(email)
-      }
-    } catch (error) {
-      setError(error?.toString());
-    }
-  }
-
-  const onSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
-    ev.preventDefault()
-    setError(null)
-    onUserUpdate({ name: nameInput, email: emailInput })
-  }
 
   return (
     <Box sx={containerStyle}>
-      <form autoComplete="off" onSubmit={onSubmit} >
+      <form autoComplete="off" >
         <TextField
           id="name"
-          value={nameInput}
-          onChange={(e) => setNameInput(e.target.value)}
+          value={name}
+          disabled
           label={tA('nameLabel')}
           variant="standard"
           fullWidth
@@ -73,29 +27,14 @@ const ProfileDetailsForm = () => {
         />
         <TextField
           id="email"
-          value={emailInput}
-          onChange={(e) => setEmailInput(e.target.value)}
+          value={email}
+          disabled
           label={tA('emailLabel')}
           variant="standard"
           type="email"
           fullWidth
           required
         />
-        <LoadingButton
-          sx={{ mt: 2, mr: 1.5, }}
-          variant="contained"
-          fullWidth
-          loading={isLoading}
-          color="secondary"
-          type="submit"
-        >
-          {tA('saveProfile')}
-        </LoadingButton>
-        {error && (
-          <Container>
-            <Typography>{error}</Typography>
-          </Container>
-        )}
       </form>
     </Box>
   );
