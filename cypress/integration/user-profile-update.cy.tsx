39795459ad7079116fcdb9489a457a4f840b13bf
Anmol <ainsa2279@gmail.com>
2024-05-17 17:49:33 +0530

    Test: add update profile test to check for name and email to be prefilled

--- cypress/integration/user-profile-update.cy.tsx
+++ cypress/integration/user-profile-update.cy.tsx
@@ -1,5 +1,6 @@
 describe('User should be able to see his profile data in settings', () => {
   const publicEmail = Cypress.env('public_email')
+  const publicName = Cypress.env('public_name')
 
   before(() => {
     cy.cleanUpTestState();
@@ -8,7 +9,8 @@ describe('User should be able to see his profile data in settings', () => {
 
   it('Navigate to the account settings page', () => {
     cy.visit('/account/settings');
-    cy.get('#email', { timeout: 10000 }).should('be.disabled')
+    cy.get('#email', { timeout: 10000 }).should('be.disabled').should('have.value', publicEmail)
+    cy.get('#name', { timeout: 10000 }).should('be.disabled').should('have.value', publicName)
     cy.wait(3000);
   });
 
