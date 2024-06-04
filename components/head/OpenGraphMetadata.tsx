211336f3c269d765452dac08ae7d7b8e8b074fbe
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2024-06-04 19:25:12 +0100

    fix: change url for bloom_social_preview

--- components/head/OpenGraphMetadata.tsx
+++ components/head/OpenGraphMetadata.tsx
@@ -6,7 +6,7 @@ const descriptionContent =
   'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
 const twitterDescriptionContent =
   'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
-const imageContent = `${BASE_URL}/bloom_socials_preview.png`;
+const imageContent = `${BASE_URL}/public/bloom_socials_preview.png`;
 const imageAltContent =
   'An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word "Bloom" hovers above the person.';
 
