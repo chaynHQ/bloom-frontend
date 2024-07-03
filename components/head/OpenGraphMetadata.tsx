e253563e3d6a26314355a0dd5f57ceced67e19a2
Anmol <ainsa2279@gmail.com>
2024-07-03 09:24:18 +0530

    Add: png image to og meta and root of project

--- components/head/OpenGraphMetadata.tsx
+++ components/head/OpenGraphMetadata.tsx
@@ -4,7 +4,7 @@ const descriptionContent =
   'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
 const twitterDescriptionContent =
   'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
-const imageContent = `/preview.webp`;
+const imageContent = `/preview_down.png`;
 const imageAltContent =
   'An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word "Bloom" hovers above the person.';
 
