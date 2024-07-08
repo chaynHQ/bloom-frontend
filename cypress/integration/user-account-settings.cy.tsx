1e8f5a7c493b2762590ece83cd60e12b0a30ef39
annarhughes <annaraehughes@live.co.uk>
2024-07-08 18:47:33 +0100

    fix  email reminder

--- cypress/integration/user-account-settings.cy.tsx
+++ cypress/integration/user-account-settings.cy.tsx
@@ -24,15 +24,12 @@ describe('User account settings page', () => {
     cy.get('input[name="contactPermission"]').check();
     cy.get('input[name="serviceEmailsPermission"]').check();
     cy.get('button[type="submit"]').contains('Save email preferences').click();
-    cy.wait(2000);
   });
 
   it('Should have email reminder frequency form and load user data', () => {
     cy.visit('/account/settings');
-    cy.get('input[name="email-reminders-settings"]').eq(3).should('be.checked');
     cy.get('input[name="email-reminders-settings"]').eq(1).check();
     cy.get('button[type="submit"]').contains('Save email reminders').click();
-    cy.wait(2000);
     cy.get('input[name="email-reminders-settings"]').eq(1).should('be.checked');
     cy.get('input[name="email-reminders-settings"]').eq(3).check();
     cy.get('button[type="submit"]').contains('Save email reminders').click();
