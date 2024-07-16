6ded83eb2dac835298b86f88a3a760efdaee50be
annarhughes <annaraehughes@live.co.uk>
2024-07-16 10:50:29 +0100

    fix tests

--- cypress/integration/user-account-settings.cy.tsx
+++ cypress/integration/user-account-settings.cy.tsx
@@ -14,31 +14,22 @@ describe('User account settings page', () => {
     cy.wait(3000);
   });
 
-  it('Should display error if tried to update email to a username that already exits', () => {
-    cy.visit('/account/settings');
-    cy.get('#email').should('have.value', publicEmail);
-    cy.get('#name').should('have.value', publicName);
-    cy.wait(3000);
-  });
-
   it('Should have marketing and service email checkbox fields and submit button', () => {
     cy.visit('/account/settings');
     cy.get('input[name="contactPermission"]').check();
     cy.get('input[name="serviceEmailsPermission"]').check();
     cy.get('button[type="submit"]').contains('Save email preferences').click();
     cy.wait(3000);
+    cy.get('input[name="contactPermission"]').eq(1).should('be.checked');
+    cy.get('input[name="serviceEmailsPermission"]').eq(1).should('be.checked');
   });
 
   it('Should have email reminder frequency form and load user data', () => {
     cy.visit('/account/settings');
-    cy.get('input[name="email-reminders-settings"]').eq(3).should('be.checked');
     cy.get('input[name="email-reminders-settings"]').eq(1).check();
     cy.get('button[type="submit"]').contains('Save email reminders').click();
     cy.wait(3000);
     cy.get('input[name="email-reminders-settings"]').eq(1).should('be.checked');
-    cy.get('input[name="email-reminders-settings"]').eq(3).check();
-    cy.get('button[type="submit"]').contains('Save email reminders').click();
-    cy.wait(3000);
   });
 
   after(() => {
