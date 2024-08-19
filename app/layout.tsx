7e5312636cc1409bc741db93e42c14dd53a5fcc5
Boodland <albarlopez@gmail.com>
2024-08-19 13:03:28 +0200

    Use new relic configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { newRelicInit } from '../config/newRelic';
 
 // Nextjs automatically includes for each route two default meta tags, charset and viewport
 // https://nextjs.org/docs/app/building-your-application/optimizing/metadata#default-fields
@@ -6,16 +7,20 @@ export const metadata: Metadata = {
   title: 'Bloom',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const NewRelicScript = await newRelicInit();
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        {NewRelicScript}
+      </body>
     </html>
   );
 }
