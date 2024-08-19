7f0deb9fad0327edead65dee028933cb434d4ab5
Boodland <albarlopez@gmail.com>
2024-08-19 11:58:28 +0200

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Bloom',
+};
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
