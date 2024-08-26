54251d9de8a5f2b47fe1c0fdc534a81b1df71ac3
Boodland <albarlopez@gmail.com>
2024-08-26 12:30:19 +0200

    Add our free services section test

--- cypress/integration/tests/home.cy.tsx
+++ cypress/integration/tests/home.cy.tsx
@@ -3,6 +3,9 @@ const HOME_PAGE_URL = '/';
 describe('Home page should display', () => {
   before(() => {
     cy.cleanUpTestState();
+  });
+
+  beforeEach(() => {
     cy.visit(HOME_PAGE_URL);
   });
 
@@ -16,6 +19,32 @@ describe('Home page should display', () => {
       'Person sitting crossed legged with symbols floating around such as a video icon, flower, message icon and a persons head with a flower',
       'landing_page_illustration',
     );
+    cy.checkLink('/auth/register', 'Get started');
+  });
+
+  it('our free services section', () => {
+    cy.get('h2').should('contain', 'Our free services');
+
+    cy.checkImage('coursesIcon', 'icon_courses');
+    cy.get('h3').should('contain', 'Online video courses');
+    cy.checkLink('/courses', '');
+
+    cy.checkImage('chatIcon', 'icon_chat');
+    cy.get('h3').should('contain', '1-to-1 Messaging');
+    cy.checkLink('/chat', '');
+
+    cy.checkImage('activityIcon', 'icon_activities');
+    cy.get('h3').should('contain', 'Reflective activities');
+    cy.checkLink('/activities', '');
+
+    cy.checkImage('groundingIcon', 'icon_grounding');
+    cy.get('h3').should('contain', 'Guided grounding activities');
+    cy.checkLink('/grounding', '');
+
+    cy.checkImage('notesIcon', 'icon_notes');
+    cy.get('h3').should('contain', 'Notes: Whatsapp messages from us');
+    cy.checkLink('/subscription/whatsapp', '');
+
     cy.checkLink('/auth/register', 'Get started');
   });
 });
