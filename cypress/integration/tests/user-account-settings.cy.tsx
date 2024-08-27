dead5f39bb63a50814c5ff846440b63de36ec039
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2024-08-27 14:48:59 +0100

    Fix: racey user account page tests

--- cypress/integration/tests/user-account-settings.cy.tsx
+++ cypress/integration/tests/user-account-settings.cy.tsx
@@ -23,13 +23,30 @@ describe('User account settings page', () => {
   });
 
   it('Should have email reminder frequency form and load user data', () => {
+    const settingsIndexMap = {
+      TWO_WEEKS: 0,
+      ONE_MONTH: 1,
+      TWO_MONTHS: 2,
+      NEVER: 3,
+    };
+
     cy.visit('/account/settings');
-    cy.get('input[name="email-reminders-settings"]', { timeout: 8000 }).eq(1).check();
-    cy.get('button[type="submit"]').contains('Save email reminders').click();
-    cy.get('input[name="email-reminders-settings"]', { timeout: 8000 }).eq(1).should('be.checked');
-    // Reset the value to 'never' so the test works on next run
-    cy.get('input[name="email-reminders-settings"]').eq(3).check();
-    cy.get('button[type="submit"]').contains('Save email reminders').click();
+    // Get currently set value
+    cy.get('input[name="email-reminders-settings"]:checked').then((item) => {
+      const currentValue: string = item.val().toString();
+      // Find the new value to be checked based on whats currently checked so tests don't fail if
+      // the default settings are changed
+      const newIndex = Object.values(settingsIndexMap).find((value) => {
+        return settingsIndexMap[currentValue] !== value;
+      });
+      cy.get('h2').contains('Email reminders').should('exist');
+      cy.get(`input[name="email-reminders-settings"]`).eq(newIndex).check();
+      cy.get('[qa-id="email-reminders-settings-submit"]', { timeout: 20000 })
+        .contains('Save email reminders')
+        .should('not.be.disabled')
+        .click();
+      cy.get('input[name="email-reminders-settings"]').eq(newIndex).should('be.checked');
+    });
   });
 
   after(() => {
