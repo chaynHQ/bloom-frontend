d98fc01a03194d406242fae443b395249e7c0b24
Boodland <albarlopez@gmail.com>
2024-09-02 17:05:52 +0200

    Add google tag manager to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import GoogleTagManagerScript from '../components/head/GoogleTagManagerScript';
 
 // Nextjs automatically includes for each route two default meta tags, charset and viewport
 // https://nextjs.org/docs/app/building-your-application/optimizing/metadata#default-fields
@@ -15,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <GoogleTagManagerScript />
+      </head>
       <body>{children}</body>
     </html>
   );
