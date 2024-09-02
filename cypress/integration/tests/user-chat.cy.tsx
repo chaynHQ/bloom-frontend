09a734a7386f7b6da58cb0f6aa21ced279125eed
Boodland <albarlopez@gmail.com>
2024-09-02 10:42:35 +0200

    Check non public section superficially

--- cypress/integration/tests/user-chat.cy.tsx
+++ cypress/integration/tests/user-chat.cy.tsx
@@ -1,4 +1,4 @@
-describe('A logged in public user can start a chat', () => {
+describe('A logged in public user can', () => {
   before(() => {
     cy.cleanUpTestState();
     cy.logInWithEmailAndPassword(
@@ -19,6 +19,16 @@ describe('A logged in public user can start a chat', () => {
     //   .and('equal', 'true'); //chatbox visible
 
     // cy.get('a[aria-label="Close chat"]').click(); //close chat box
+  });
+
+  it('see the More about section', () => {
+    cy.visit('/chat');
+    cy.get('h2').contains('More about how 1-1 Chat works');
+    cy.checkImage('alt', 'illustration_leaf_mix');
+    cy.get('p').contains('Who am I speaking to? Am I always chatting with the same person?');
+    cy.get('p').contains(
+      'We are a group of survivors and allies based all over the world who care about your healing journey. Because of our different time zones and the way we run our chat service, you won’t always be speaking with the same person. You can find out more about our team and who we are',
+    );
   });
 
   after(() => {
