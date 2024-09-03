6533bf17f119307a3342e636fd48536955ec9f5b
Boodland <albarlopez@gmail.com>
2024-09-03 17:10:34 +0200

    Fix format

--- app/rootMetadata.ts
+++ app/rootMetadata.ts
@@ -2,31 +2,31 @@ import { Metadata } from 'next';
 import { PRIMARY_MAIN_COLOR } from '../constants/common';
 
 const descriptionContent =
-    'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
+  'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
 const twitterDescriptionContent =
-    'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
+  'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
 const imageAltContent =
-    'An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word "Bloom" hovers above the person.';
+  'An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word "Bloom" hovers above the person.';
 
 // Nextjs automatically includes for each route two default meta tags, charset and viewport
 // https://nextjs.org/docs/app/building-your-application/optimizing/metadata#default-fields
 const rootMetadata: Metadata = {
-    title: 'Bloom',
-    openGraph: {
-        title: 'Welcome to Bloom',
-        description: descriptionContent,
-        images: [{ url: '/preview.png', alt: imageAltContent }],
-    },
-    twitter: {
-        description: twitterDescriptionContent,
-        card: 'summary_large_image',
-        images: [],
-    },
-    manifest: '/manifest.json',
-    icons: [{ rel: 'apple-touch-icon', url: '/icons/apple/icon-120x120.png' }],
-    other: {
-        'theme-color': PRIMARY_MAIN_COLOR,
-    },
+  title: 'Bloom',
+  openGraph: {
+    title: 'Welcome to Bloom',
+    description: descriptionContent,
+    images: [{ url: '/preview.png', alt: imageAltContent }],
+  },
+  twitter: {
+    description: twitterDescriptionContent,
+    card: 'summary_large_image',
+    images: [],
+  },
+  manifest: '/manifest.json',
+  icons: [{ rel: 'apple-touch-icon', url: '/icons/apple/icon-120x120.png' }],
+  other: {
+    'theme-color': PRIMARY_MAIN_COLOR,
+  },
 };
 
 export default rootMetadata;
