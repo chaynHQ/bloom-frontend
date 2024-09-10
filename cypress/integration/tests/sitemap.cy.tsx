3636d7d7fcd55c26a39c4aeb912812abf3da8f31
Boodland <albarlopez@gmail.com>
2024-09-10 17:13:41 +0200

    Simplify tests cases after review feedback

--- cypress/integration/tests/sitemap.cy.tsx
+++ cypress/integration/tests/sitemap.cy.tsx
@@ -8,32 +8,9 @@ describe('Sitemap should have', () => {
   it('bloom url', () => {
     cy.request(SITEMAP_URL).its('body').should('include', BLOOM_URL);
   });
-  it('about our courses url', () => {
-    cy.request(SITEMAP_URL)
-      .its('body')
-      .should('include', BLOOM_URL + '/about-our-courses');
-  });
   it('meet the team url', () => {
     cy.request(SITEMAP_URL)
       .its('body')
       .should('include', BLOOM_URL + '/meet-the-team');
-  });
-  it('therapy urls', () => {
-    const THERAPY_SEGMENT = '/therapy';
-    cy.request(SITEMAP_URL)
-      .its('body')
-      .should('include', BLOOM_URL + THERAPY_SEGMENT + '/book-session')
-      .should('include', BLOOM_URL + THERAPY_SEGMENT + '/confirmed-session');
-  });
-  it('courses urls', () => {
-    const COURSES_SEGMENT = '/courses';
-    const localeSegments = ['', '/es', '/hi', '/fr', '/pt', '/de'];
-
-    localeSegments.forEach((localeSegment) => {
-      cy.request(SITEMAP_URL)
-        .its('body')
-        .should('include', BLOOM_URL + localeSegment + COURSES_SEGMENT)
-        .should('have.length.greaterThan', 0);
-    });
   });
 });
