36b13ee06c1ad93dfb87bdcd5a4798eba28efb41
annarhughes <annaraehughes@live.co.uk>
2024-09-11 17:16:55 +0100

    improve quote block styles

--- components/storyblok/StoryblokQuote.tsx
+++ components/storyblok/StoryblokQuote.tsx
@@ -3,10 +3,21 @@ import { ISbRichtext, storyblokEditable } from '@storyblok/react';
 import Image from 'next/image';
 import Quote from '../common/Quote';
 
+const containerStyle = {
+  display: 'flex',
+  marginX: { xs: 2, lg: 3 },
+  flexDirection: { xs: 'column', md: 'row' },
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  gap: { xs: 2, md: 10 },
+  paddingTop: 1,
+} as const;
+
 const imageContainerStyle = {
   marginX: 'auto',
   marginY: 4,
-  width: { xs: 200, md: 250 },
+  minWidth: { xs: 200, md: 230, lg: 260 },
+  maxWidth: { xs: 200, md: 230, lg: 260 },
   '.image': {
     objectFit: 'contain',
     width: '100% !important',
@@ -32,17 +43,11 @@ const StoryblokQuote = (props: StoryblokQuoteProps) => {
   return (
     <Box
       {...storyblokEditable({ _uid, _editable, text, text_size, icon_color, image })}
-      sx={{
-        display: 'flex',
-        flexDirection: { xs: 'column', md: 'row' },
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: 7,
-      }}
+      sx={containerStyle}
     >
       {image && (
         <Box sx={imageContainerStyle}>
-          <Image src={image.filename} alt={image.alt} className="image" fill sizes="100vw" />
+          <Image src={image.filename} alt={image.alt} className="image" fill sizes="520px" />
         </Box>
       )}
       <Quote text={text} textSize={text_size} iconColor={icon_color} />
