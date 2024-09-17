50dec7b972c15b75f583f9aa46ba569b4cc17d0a
Alberto Arias <albarlopez@gmail.com>
2024-09-17 17:59:11 +0200

    App router migration google tag manager script (#1117)
    * Add basic Root Layout
    
    * Add metadata
    
    * Add reference included by next
    
    * Add comment to explain the default meta tags
    
    * Add public testing page to check the migration
    
    * Remove non required file
    
    * Add google tag manager to root layout
    
    * Change strategy as worker is experimental and is not including the script
    
    * Move gtm to body as next is including it in head properly
    
    * Add comment to tackle in the future
    
    * Move gtm script from _document to _app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import GoogleTagManagerScript from '../components/head/GoogleTagManagerScript';
 
 // Nextjs automatically includes for each route two default meta tags, charset and viewport
 // https://nextjs.org/docs/app/building-your-application/optimizing/metadata#default-fields
@@ -15,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* 
+        We should be using next third party library https://nextjs.org/docs/app/building-your-application/optimizing/third-party-libraries#google-tag-manager 
+        but sending an event using sendGTMEvent requires an object rather than a list of arguments so the current gtag api function would need to be adapted
+        */}
+        <GoogleTagManagerScript />
+        {children}
+      </body>
     </html>
   );
 }
