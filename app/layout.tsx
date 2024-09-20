a79c735392d3cbcee3339999460f40d87feff3da
Alberto Arias <albarlopez@gmail.com>
2024-09-20 15:55:24 +0200

    App router migration material UI configuration (#1122)
    * Add basic Root Layout
    
    * Add metadata
    
    * Add reference included by next
    
    * Add comment to explain the default meta tags
    
    * Add public testing page to check the migration
    
    * Remove non required file
    
    * Create theme registry component
    
    * Add ThemeRegistry to root layout
    
    * Make theme client component
    
    * Install mui for app router
    
    ---------
    
    Co-authored-by: eleanorreem <eleanor.reem@hotmail.co.uk>

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import GoogleTagManagerScript from '../components/head/GoogleTagManagerScript';
 import ErrorBoundary from '../components/layout/ErrorBoundary';
 import rootMetadata from './rootMetadata';
 import RollbarScript from '../components/head/RollbarScript';
+import ThemeRegistry from './ThemeRegistry';
+import rootMetadata from './rootMetadata';
 
 export const metadata = rootMetadata;
 
@@ -21,7 +23,9 @@ export default function RootLayout({
         */}
         <GoogleTagManagerScript />
         <RollbarScript />
-        <ErrorBoundary>{children}</ErrorBoundary>
+        <ErrorBoundary>
+          <ThemeRegistry>{children}</ThemeRegistry>
+        </ErrorBoundary>
       </body>
     </html>
   );
