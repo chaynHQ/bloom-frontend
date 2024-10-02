39b61d422014f41ff2e68446d7bba850e679a643
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2024-10-02 13:59:28 +0200

    fix: access to course pages

--- components/storyblok/StoryblokCoursePage.tsx
+++ components/storyblok/StoryblokCoursePage.tsx
@@ -89,8 +89,11 @@ const StoryblokCoursePage = (props: StoryblokCoursePageProps) => {
 
   useEffect(() => {
     const storyPartners = included_for_partners;
+    const referralPartner = window.localStorage.getItem('referralPartner');
 
-    setIncorrectAccess(!hasAccessToPage(storyPartners, partnerAccesses, partnerAdmin));
+    setIncorrectAccess(
+      !hasAccessToPage(true, storyPartners, partnerAccesses, partnerAdmin, referralPartner),
+    );
   }, [partnerAccesses, partnerAdmin, included_for_partners]);
 
   useEffect(() => {
