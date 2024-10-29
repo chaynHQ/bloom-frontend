3b0367bb101b8df2c0a349aa542b33e08da82014
annarhughes <annaraehughes@live.co.uk>
2024-10-29 09:12:04 +0000

    fix tests

---
+++ cypress/integration/tests/user-messaging.cy.tsx
@@ -0,0 +1,31 @@
+describe('A logged in public user can', () => {
+  const email = `cypresstestemail+${Date.now()}@chayn.co`;
+  const password = 'testtesttest';
+
+  before(() => {
+    cy.cleanUpTestState();
+    cy.createUser({ emailInput: email, passwordInput: password });
+    cy.logInWithEmailAndPassword(email, password);
+  });
+
+  it('Navigate to the messaging page and begin a messaging', () => {
+    cy.visit('/'); //intitial home page visit
+    cy.get(`[qa-id=secondary-nav-messaging-button]`, { timeout: 8000 }).should('exist').click(); //go to messaging page
+  });
+
+  it('see the More about section', () => {
+    cy.visit('/messaging');
+    cy.get('h2').contains('Tell us what’s on your mind');
+    cy.get('h2').contains('Meet the Bloom team');
+    cy.get('h2').contains('More about how Bloom’s Messaging works');
+    cy.checkImage('alt', 'Illustration of a person sitting');
+    cy.get('p').contains('Who is 1-1 Messaging for?');
+    cy.get('p').contains(
+      '1-1 Messaging is available for everyone using Bloom, regardless of your background, race, age, disability, religion or belief, sexuality, gender identity or expression, or life circumstances—we are here for you.',
+    );
+  });
+
+  after(() => {
+    cy.logout();
+  });
+});
