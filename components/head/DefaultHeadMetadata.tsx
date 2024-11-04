42492dba604a7dd42a387b75193d4ae8cdb2a6f6
annarhughes <annaraehughes@live.co.uk>
2024-11-04 17:52:35 +0000

    translate default meta strings

--- components/head/DefaultHeadMetadata.tsx
+++ components/head/DefaultHeadMetadata.tsx
@@ -1,20 +1,17 @@
+import { useTranslations } from 'next-intl';
 import Head from 'next/head';
 import theme from '../../styles/theme';
 
-const titleContent = 'Welcome to Bloom';
-const descriptionContent =
-  'Join us on your healing journey. Bloom is here for you to learn, heal and grow towards a confident future. It is bought to you by Chayn, a global non-profit, run by survivors and allies from around the world.';
-const imageAltContent =
-  'An cartoon drawing of a person with almost shoulder length hair against a pink background. They have flowers and leaves coming out of their head. The word "Bloom" hovers above the person.';
-
 const DefaultHeadMetadata = () => {
+  const t = useTranslations('Shared.metadata');
+
   return (
     <Head>
       <meta name="viewport" content="initial-scale=1, width=device-width" />
-      <meta property="og:title" content={titleContent} key="og-title" />
-      <meta property="og:description" content={descriptionContent} key="og-description" />
+      <meta property="og:title" content={t('title')} key="og-title" />
+      <meta property="og:description" content={t('description')} key="og-description" />
       <meta property="og:image" content="/preview.png" key="og-image" />
-      <meta property="og:image:alt" content={imageAltContent} key="og-image-alt" />
+      <meta property="og:image:alt" content={t('imageAlt')} key="og-image-alt" />
       <meta name="twitter:card" content="summary_large_image" key="twitter-card" />
       <meta name="twitter:site" content="@ChaynHQ" />
       <meta name="twitter:creator" content="@ChaynHQ" />
