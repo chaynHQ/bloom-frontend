50431ea8a55b6d02ba8824cbabc88902d04fbb1d
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2024-12-28 16:49:31 +0000

    clean up user-disable sesrvice email test

--- cypress/integration/tests/user-disable-service-emails.cy.tsx
+++ cypress/integration/tests/user-disable-service-emails.cy.tsx
@@ -6,24 +6,9 @@ describe('User disable service emails page should display', () => {
     cy.logInWithEmailAndPassword(publicEmail, Cypress.env('CYPRESS_PUBLIC_PASSWORD'));
   });
 
-  beforeEach(() => {
-    cy.visit('/account/disable-service-emails');
-  });
-
   it('header section', () => {
+    cy.visit('/account/disable-service-emails');
     cy.get('h1').contains('Bloom emails turned off');
     cy.checkImage('Account.disableServiceEmails.imageAlt', 'illustration_leaf_mix_bee');
-    cy.get('p').should(
-      'contain',
-      `You will no longer receive emails related to Bloom. If you'd like to change this later, please get in touch with the`,
-    );
-    cy.checkLink(
-      'https://chayn.typeform.com/to/OY9Wdk4h?typeform-source=bloom.chayn.co',
-      'Bloom team',
-    );
-  });
-
-  after(() => {
-    cy.logout();
   });
 });
