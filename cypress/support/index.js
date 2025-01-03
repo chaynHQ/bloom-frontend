8afadd1a0cf7ed29ffee2a20c0f5e4b9b87e8a54
Anna Hughes <annaraehughes@live.co.uk>
2025-01-03 18:08:16 +0000

    fix: therapy usage test + after cleanup (#1258)

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,16 +5,3 @@ const customCommands = require('./commands.js');
 module.exports = {
   commands: customCommands,
 };
-
-// Calls backend API route to delete cypress users
-// Ideally this would be called once after ALL suites are run
-// For now we call the API route after every suite is run
-after(() => {
-  const superAdminEmail = Cypress.env('CYPRESS_SUPER_ADMIN_EMAIL');
-  const superAdminPassword = Cypress.env('CYPRESS_SUPER_ADMIN_PASSWORD');
-  cy.logInWithEmailAndPassword(superAdminEmail, superAdminPassword);
-
-  cy.deleteAllCypressUsers().then(() => {
-    cy.log('After function completed - stale cypress users deleted');
-  });
-});
