48d0197d2a3bc9f79aad822593865344308e1bd8
annarhughes <annaraehughes@live.co.uk>
2025-01-21 22:21:58 +0000

    fix build errors

--- hooks/useReferralPartner.ts
+++ hooks/useReferralPartner.ts
@@ -1,13 +1,13 @@
 import Cookies from 'js-cookie';
 import { useEffect } from 'react';
-import { useRouter } from '../i18n/routing';
+import { usePathname } from '../i18n/routing';
 import { setEntryPartnerAccessCode, setEntryPartnerReferral } from '../lib/store/userSlice';
 import { useAppDispatch, useTypedSelector } from './store';
 
 // Check if entry path is from a partner referral and if so, store referring partner and code in state and local storage
 // This enables us to redirect a user to the correct sign up page later (e.g. in SignUpBanner)
 export default function useReferralPartner() {
-  const router = useRouter();
+  const pathname = usePathname();
   const dispatch = useAppDispatch();
   const userCookiesAccepted =
     useTypedSelector((state) => state.user.cookiesAccepted) ||
@@ -22,12 +22,10 @@ export default function useReferralPartner() {
       await dispatch(setEntryPartnerAccessCode(referralCode));
     }
 
-    const path = router.asPath;
-
     let referralPartner, referralCode;
 
-    if (path?.includes('/welcome/')) {
-      const splitPartnerQuery = path.split('/')[2].split('?');
+    if (pathname?.includes('/welcome/')) {
+      const splitPartnerQuery = pathname.split('/')[2].split('?');
       referralPartner = splitPartnerQuery[0]; // Gets "bumble" from /welcome/bumble?code=123
 
       if (splitPartnerQuery.length === 2) {
@@ -37,8 +35,8 @@ export default function useReferralPartner() {
         }
       }
     }
-    if (path?.includes('/register')) {
-      const splitPartnerQuery = path.split('?'); // Gets "partner=bumble&code=123" from /register?partner=bumble&code=123
+    if (pathname?.includes('/register')) {
+      const splitPartnerQuery = pathname.split('?'); // Gets "partner=bumble&code=123" from /register?partner=bumble&code=123
       if (splitPartnerQuery.length === 2) {
         const splitCodeQuery = splitPartnerQuery[1].split('&'); // Gets "partner=bumble" and "code=123"
 
