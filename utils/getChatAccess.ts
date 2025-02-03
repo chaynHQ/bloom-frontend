f5bb34130842ba16d8b7d6d931b149c3bfdd29ca
RaulUrbina <raul.urbina@encora.com>
2025-02-03 13:38:59 -0600

    remove liveCourseAccess references (#1286)

--- utils/getChatAccess.ts
+++ utils/getChatAccess.ts
@@ -1,13 +1,10 @@
 import { Dispatch, SetStateAction } from 'react';
 import { PartnerAccesses } from '../store/partnerAccessSlice';
-import { PartnerAdmin } from '../store/partnerAdminSlice';
 
 export const getChatAccess = (
   partnerAccesses: PartnerAccesses,
   setLiveChatAccess: Dispatch<SetStateAction<boolean>>,
-  partnerAdmin: PartnerAdmin,
 ) => {
-  const liveCourseAccess = partnerAccesses.length === 0 && !partnerAdmin.id;
   const liveAccess = partnerAccesses.find((partnerAccess) => partnerAccess.featureLiveChat);
-  if (liveAccess || liveCourseAccess) setLiveChatAccess(true);
+  if (liveAccess) setLiveChatAccess(true);
 };
