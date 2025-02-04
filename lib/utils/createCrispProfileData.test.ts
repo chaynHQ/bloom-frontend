a015cddcd63f01c45ce067474aaed3d79f090ba9
annarhughes <annaraehughes@live.co.uk>
2025-02-04 16:12:34 +0000

    redo imports

--- lib/utils/createCrispProfileData.test.ts
+++ lib/utils/createCrispProfileData.test.ts
@@ -1,7 +1,7 @@
-import { STORYBLOK_STORY_STATUS } from '@/lib/constants/enums';
-import { Course, Session } from '@/lib/store/coursesSlice';
-import { PartnerAccess } from '@/lib/store/partnerAccessSlice';
 import { expect } from '@jest/globals';
+import { STORYBLOK_STORY_STATUS } from '../constants/enums';
+import { Course, Session } from '../store/coursesSlice';
+import { PartnerAccess } from '../store/partnerAccessSlice';
 import { createCrispProfileData } from './createCrispProfileData';
 
 const partnerAccess = {
