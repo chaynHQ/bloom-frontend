7c1e67bccba90f352ea873f4051bb0426d6281b5
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2025-02-09 23:13:26 +0000

    fix: cypress tests for auth redirect

--- cypress/integration/tests/auth-redirect.cy.tsx
+++ cypress/integration/tests/auth-redirect.cy.tsx
@@ -2,12 +2,12 @@ describe('Auth redirect', () => {
   before(() => {
     cy.cleanUpTestState();
   });
-  it('User visits a session page with an auth guard and should be redirected', () => {
+  it('User visits a session page with an auth guard and should be shown a pop up login dialog', () => {
     cy.visit(
       '/courses/image-based-abuse-and-rebuilding-ourselves/the-social-context-of-image-based-abuse-and-victim-blaming',
       { failOnStatusCode: false },
     );
-
+    cy.get('[qa-id="dialogLoginButton"]').should('contain', 'Log in').click();
     cy.get('h1').should('contain', 'Welcome back');
     cy.url().should(
       'include',
