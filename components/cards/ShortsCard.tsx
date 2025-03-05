e38c619e62a1fad7e094b2d78b444a8264899151
illmix <illachaban1100@gmail.com>
2025-03-05 19:57:53 +0200

    add overlay with play button

--- components/cards/ShortsCard.tsx
+++ components/cards/ShortsCard.tsx
@@ -2,17 +2,38 @@
 
 import { Link as i18nLink } from '@/i18n/routing';
 import { RELATED_CONTENT_CATEGORIES } from '@/lib/constants/enums';
-import { Box, Card, CardActionArea, CardContent, Typography } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, IconButton, Typography } from '@mui/material';
 import { useTranslations } from 'next-intl';
 import Image from 'next/image';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 const cardStyle = {
   mt: 0,
   width: '250px',
   mb: { xs: '1rem', sm: '1.5rem' },
   backgroundColor: 'paleSecondaryLight',
+  '&:hover .overlay': {
+    opacity: 1,
+  },
+  '&:hover .play-button': {
+    transform: 'scale(1)',
+  },
   flex: 0,
 } as const;
+
+const overlay = {
+  position: 'absolute',
+  right: 0,
+  bottom: 0,
+  top: 0,
+  left: 0,
+  opacity: 0,
+  transition: 'opacity .3s ease',
+  backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
 
 interface ShortsCardProps {
   title: string;
@@ -56,6 +77,21 @@ export const ShortsCard = (props: ShortsCardProps) => {
               fill
               alt={image?.alt || 'Bloom shorts default image'} // TODO create a message for this image
             />
+            <Box
+              className="overlay"
+              sx={overlay}>
+              <IconButton
+                className="play-button"
+                sx={{
+                  color: 'white',
+                  backgroundColor: 'rgb(255, 250, 250)',
+                  transform: 'scale(0.75)',
+                  transition: 'transform .3s ease',
+                }}
+              >
+                <PlayArrowIcon sx={{ fontSize: 40, color: 'error.main' }} />
+              </IconButton>
+            </Box>
           </Box>
           <Box minHeight="100px" p={3}>
             <Typography sx={categoryStyle}>
