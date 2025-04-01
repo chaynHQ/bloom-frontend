1168386879c3e7566145bd369300c19f95ddae6c
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2025-04-01 16:54:39 +0100

    feat: grounding exercise event log

--- components/storyblok/StoryblokVideo.tsx
+++ components/storyblok/StoryblokVideo.tsx
@@ -1,38 +1,25 @@
 'use client';
 
+import { useTypedSelector } from '@/lib/hooks/store';
+import { getEventUserData } from '@/lib/utils/logEvent';
 import { richtextContentStyle } from '@/styles/common';
-import { Box } from '@mui/material';
-import { storyblokEditable } from '@storyblok/react/rsc';
-import dynamic from 'next/dynamic';
-import { YouTubeConfig } from 'react-player/youtube';
-import { videoContainerStyle, videoStyle } from '../video/Video';
-// See React Player Hydration issue https://github.com/cookpete/react-player/issues/1474
-const ReactPlayer = dynamic(() => import('react-player/youtube'), { ssr: false });
-
-const getVideoConfig = (video: { url: string }): YouTubeConfig | undefined => {
-  return video.url.indexOf('youtu.be') > -1 || video.url.indexOf('youtube') > -1
-    ? {
-        embedOptions: {
-          host: 'https://www.youtube-nocookie.com',
-        },
-      }
-    : undefined;
-};
-
+import Video from '../video/Video';
 interface StoryblokVideoProps {
   _uid: string;
   _editable: string;
   video: { url: string };
   size: string;
   alignment: string;
+  title?: string;
 }
 
 const StoryblokVideo = (props: StoryblokVideoProps) => {
-  const { _uid, _editable, video, size = 'extra-large', alignment = 'left' } = props;
-
+  const { video, title, size = 'extra-large', alignment = 'left' } = props;
+  const userCreatedAt = useTypedSelector((state) => state.user.createdAt);
+  const partnerAccesses = useTypedSelector((state) => state.partnerAccesses);
+  const partnerAdmin = useTypedSelector((state) => state.partnerAdmin);
+  const eventUserData = getEventUserData(userCreatedAt, partnerAccesses, partnerAdmin);
   if (!video) return <></>;
-
-  const videoConfig = getVideoConfig(video);
 
   const containerStyle = {
     maxWidth: 514, // <515px prevents the "Watch on youtube" button
@@ -63,20 +50,13 @@ const StoryblokVideo = (props: StoryblokVideoProps) => {
   } as const;
 
   return (
-    <Box sx={containerStyle} {...storyblokEditable({ _uid, _editable, video, size, alignment })}>
-      <Box sx={videoContainerStyle}>
-        <ReactPlayer
-          style={videoStyle}
-          width="100%"
-          height="100%"
-          light={true}
-          url={video.url}
-          controls
-          modestbranding={1}
-          config={videoConfig}
-        />
-      </Box>
-    </Box>
+    <Video
+      url={video.url}
+      eventPrefix="STORYBLOK"
+      eventData={eventUserData}
+      title={title}
+      containerStyles={containerStyle}
+    />
   );
 };
 
