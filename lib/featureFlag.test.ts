4211f8769c5eb09ec8c14903079e0a8a23875914
Daniel Ntege <danientege785@gmail.com>
2025-04-23 08:38:20 +0300

    refactor: remove disabled courses tests from featureFlag test suite

--- lib/featureFlag.test.ts
+++ lib/featureFlag.test.ts
@@ -1,22 +1,7 @@
 import { expect } from '@jest/globals';
 import { FeatureFlag } from './featureFlag';
 
-const disabledCourse1 = 'fr/courses/healing-from-sexual-trauma/';
-const disabledCourse2 = 'fr/courses/dating-boundaries-and-relationships/';
-
 describe('featureFlag', () => {
-  it('should return disabled courses when environment variable set', () => {
-    process.env.FF_DISABLED_COURSES = [disabledCourse1, disabledCourse2].join(',');
-
-    expect(Array.from(FeatureFlag.getDisabledCourses())).toContain(disabledCourse1);
-    expect(Array.from(FeatureFlag.getDisabledCourses())).toContain(disabledCourse2);
-  });
-
-  it('should return empty array when environment variable not', () => {
-    process.env.FF_DISABLED_COURSES = '';
-
-    expect(FeatureFlag.getDisabledCourses().size).toEqual(0);
-  });
 
   it('should return user research banner value when environment variable set', () => {
     process.env.NEXT_PUBLIC_FF_USER_RESEARCH_BANNER = 'true';
