400507277b2bf5b57f4a68c339e6a9b83261bd5e
annarhughes <annaraehughes@live.co.uk>
2025-12-22 16:18:16 +0000

    skip cleanup

--- cypress/integration/tests/_cleanup.cy.tsx
+++ cypress/integration/tests/_cleanup.cy.tsx
@@ -1,7 +1,9 @@
 // This file runs first (alphabetically) to clean up stale test users before the test suite
 // This ensures tests start with a clean state
+// Currently skipped due to superadmin requiring MFA which we cant emulate in cypress. Solution would be to remove
+// the superadmin auth guard and require a specific cypress admin account for this endpoint on the backend.
 
-describe('Pre-test cleanup', () => {
+describe.skip('Pre-test cleanup', () => {
   it('should delete all stale cypress test users', () => {
     const superAdminEmail = Cypress.env('CYPRESS_SUPER_ADMIN_EMAIL');
     const superAdminPassword = Cypress.env('CYPRESS_SUPER_ADMIN_PASSWORD');
