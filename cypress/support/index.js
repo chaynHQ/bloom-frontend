1d995a2b24c123810ecd02d59b0f7fd7462bda74
annarhughes <annaraehughes@live.co.uk>
2026-04-15 19:46:52 +0200

    VERCEL_AUTOMATION_BYPASS_SECRET

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,6 +2,14 @@
 import '@cypress/code-coverage/support';
 const customCommands = require('./commands.js');
 
+// Bypass Vercel Deployment Protection for automated tests
+const bypassSecret = Cypress.env('VERCEL_BYPASS_SECRET');
+if (bypassSecret) {
+  beforeEach(() => {
+    cy.setCookie('x-vercel-protection-bypass', bypassSecret);
+  });
+}
+
 module.exports = {
   commands: customCommands,
 };
